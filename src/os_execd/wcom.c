adamnugraha/wazuh

Remove call to execvpe()

src/os_execd/wcom.c
@@ -459,7 +459,7 @@ size_t wcom_upgrade(const char * package, const char * installer, char * output)
     }
 #endif
 
-    if (wm_exec(installer_j, &out, &status, timeout) < 0) {
+    if (wm_exec(installer_j, &out, &status, timeout, NULL) < 0) {
         merror("At WCOM upgrade: Error executing command [%s]", installer_j);
         strcpy(output, "err Cannot execute installer");
         return strlen(output);

wazuh/wazuh-agent

src/os_execd/wcom.c
@@ -477,7 +477,7 @@ size_t wcom_upgrade(const char * package, const char * installer, char ** output
     }
 #endif
 
-    if (wm_exec(installer_j, &out, &status, req_timeout) < 0) {
+    if (wm_exec(installer_j, &out, &status, req_timeout, NULL) < 0) {
         merror("At WCOM upgrade: Error executing command [%s]", installer_j);
         *output = strdup("err Cannot execute installer");
         return strlen(*output);