wazuh/wazuh

Add check_attributes option in Syscheck for Windows

src/headers/syscheck_op.h
@@ -20,6 +20,26 @@
 #include <grp.h>
 #define PATH_SEP '/'
 
+#define FILE_ATTRIBUTE_READONLY                0x00000001
+#define FILE_ATTRIBUTE_HIDDEN                  0x00000002
+#define FILE_ATTRIBUTE_SYSTEM                  0x00000004
+#define FILE_ATTRIBUTE_DIRECTORY               0x00000010
+#define FILE_ATTRIBUTE_ARCHIVE                 0x00000020
+#define FILE_ATTRIBUTE_DEVICE                  0x00000040
+#define FILE_ATTRIBUTE_NORMAL                  0x00000080
+#define FILE_ATTRIBUTE_TEMPORARY               0x00000100
+#define FILE_ATTRIBUTE_SPARSE_FILE             0x00000200
+#define FILE_ATTRIBUTE_REPARSE_POINT           0x00000400
+#define FILE_ATTRIBUTE_COMPRESSED              0x00000800
+#define FILE_ATTRIBUTE_OFFLINE                 0x00001000
+#define FILE_ATTRIBUTE_NOT_CONTENT_INDEXED     0x00002000
+#define FILE_ATTRIBUTE_ENCRYPTED               0x00004000
+#define FILE_ATTRIBUTE_INTEGRITY_STREAM        0x00008000
+#define FILE_ATTRIBUTE_VIRTUAL                 0x00010000
+#define FILE_ATTRIBUTE_NO_SCRUB_DATA           0x00020000
+#define FILE_ATTRIBUTE_RECALL_ON_OPEN          0x00040000
+#define FILE_ATTRIBUTE_RECALL_ON_DATA_ACCESS   0x00400000
+
 #else
 
 #include "shared.h"
@@ -43,6 +63,7 @@ typedef enum sk_syscheck {
     SK_GNAME,
     SK_INODE,
     SK_SHA256,
+    SK_ATTRS,
     SK_MTIME,
     SK_CHFIELDS,
     SK_USER_ID,
@@ -73,6 +94,7 @@ typedef struct __sdb {
     char sha256[OS_FLSIZE + 1];
     char mtime[OS_FLSIZE + 1];
     char inode[OS_FLSIZE + 1];
+    char attrs[OS_SIZE_1024 + 1];
 
     char agent_cp[MAX_AGENTS + 1][1];
     char *agent_ips[MAX_AGENTS + 1];
@@ -131,6 +153,7 @@ typedef struct sk_sum_t {
     char *md5;
     char *sha1;
     char *sha256;
+    unsigned int attrs;
     char *uname;
     char *gname;
     long mtime;
@@ -166,10 +189,12 @@ void normalize_path(char *path);
 
 const char *get_user(__attribute__((unused)) const char *path, int uid, __attribute__((unused)) char **sid);
 const char* get_group(int gid);
+void get_attributes_str(char *str, unsigned int attrs);
 
 #else
 
 const char *get_user(const char *path, __attribute__((unused)) int uid, char **sid);
+unsigned int get_attrs(const char *file_path);
 const char *get_group(__attribute__((unused)) int gid);
 
 #endif

src/shared/syscheck_op.c
@@ -25,7 +25,7 @@ int sk_decode_sum(sk_sum_t *sum, char *c_sum, char *w_sum) {
     char *c_perm;
     char *c_mtime;
     char *c_inode;
-    char *tag;
+    char *attrs;
     int retval = 0;
 
     memset(sum, 0, sizeof(sk_sum_t));
@@ -86,12 +86,9 @@ int sk_decode_sum(sk_sum_t *sum, char *c_sum, char *w_sum) {
             if ((sum->sha256 = strchr(c_inode, ':')))
                 *(sum->sha256++) = '\0';
 
-            /* Look for a defined tag */
-            if (sum->sha256) {
-                if (tag = strchr(sum->sha256, ':'), tag) {
-                    *(tag++) = '\0';
-                    sum->tag = tag;
-                }
+            if ((attrs = strchr(sum->sha256, ':'))) {
+                *(attrs++) = '\0';
+                sum->attrs = strtoul(attrs, NULL, 10);
             }
 
             sum->mtime = atol(c_mtime);
@@ -466,6 +463,35 @@ const char* get_group(int gid) {
     return group ? group->gr_name : "";
 }
 
+void get_attributes_str(char *str, unsigned int attrs) {
+    size_t size;
+
+    size = snprintf(str, OS_SIZE_1024, "%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s",
+                    attrs & FILE_ATTRIBUTE_ARCHIVE ? "ARCHIVE ," : "",
+                    attrs & FILE_ATTRIBUTE_COMPRESSED ? "COMPRESSED ," : "",
+                    attrs & FILE_ATTRIBUTE_DEVICE ? "DEVICE ," : "",
+                    attrs & FILE_ATTRIBUTE_DIRECTORY ? "DIRECTORY ," : "",
+                    attrs & FILE_ATTRIBUTE_ENCRYPTED ? "ENCRYPTED ," : "",
+                    attrs & FILE_ATTRIBUTE_HIDDEN ? "HIDDEN ," : "",
+                    attrs & FILE_ATTRIBUTE_INTEGRITY_STREAM ? "INTEGRITY_STREAM ," : "",
+                    attrs & FILE_ATTRIBUTE_NORMAL ? "NORMAL ," : "",
+                    attrs & FILE_ATTRIBUTE_NOT_CONTENT_INDEXED ? "NOT_CONTENT_INDEXED ," : "",
+                    attrs & FILE_ATTRIBUTE_NO_SCRUB_DATA ? "NO_SCRUB_DATA ," : "",
+                    attrs & FILE_ATTRIBUTE_OFFLINE ? "OFFLINE ," : "",
+                    attrs & FILE_ATTRIBUTE_READONLY ? "READONLY ," : "",
+                    attrs & FILE_ATTRIBUTE_RECALL_ON_DATA_ACCESS ? "RECALL_ON_DATA_ACCESS ," : "",
+                    attrs & FILE_ATTRIBUTE_RECALL_ON_OPEN ? "RECALL_ON_OPEN ," : "",
+                    attrs & FILE_ATTRIBUTE_REPARSE_POINT ? "REPARSE_POINT ," : "",
+                    attrs & FILE_ATTRIBUTE_SPARSE_FILE ? "SPARSE_FILE ," : "",
+                    attrs & FILE_ATTRIBUTE_SYSTEM ? "SYSTEM ," : "",
+                    attrs & FILE_ATTRIBUTE_TEMPORARY ? "TEMPORARY ," : "",
+                    attrs & FILE_ATTRIBUTE_VIRTUAL ? "VIRTUAL ," : ""
+    );
+    if (size > 1) {
+        str[size - 2] = '\0';
+    }
+}
+
 #else
 
 const char *get_user(const char *path, __attribute__((unused)) int uid, char **sid)
@@ -575,6 +601,17 @@ end:
         LocalFree(pSD);
     }
     return AcctName;
+}
+
+unsigned int get_attrs(const char *file_path) {
+    unsigned int attrs;
+
+    if (attrs = GetFileAttributesA(file_path), attrs == INVALID_FILE_ATTRIBUTES) {
+        attrs = 0;
+        merror("The attributes for '%s' could not be obtained. Error '%ld'.", file_path, GetLastError());
+    }
+
+    return attrs;
 }
 
 const char *get_group(__attribute__((unused)) int gid) {

src/syscheckd/create_db.c
@@ -296,7 +296,7 @@ static int read_file(const char *file_name, int dir_position, whodata_evt *evt, 
                 alertdump = seechanges_addfile(file_name);
             }
 #ifdef WIN32
-            snprintf(alert_msg, OS_MAXSTR, "%c%c%c%c%c%c%c%c%c%c%ld:%d:%s::%s:%s:%s:%s:%ld:%ld:%s",
+            snprintf(alert_msg, OS_MAXSTR, "%c%c%c%c%c%c%c%c%c%c%c%ld:%d:%s::%s:%s:%s:%s:%ld:%ld:%s:%u",
                     opts & CHECK_SIZE ? '+' : '-',
                     opts & CHECK_PERM ? '+' : '-',
                     opts & CHECK_OWNER ? '+' : '-',
@@ -306,6 +306,7 @@ static int read_file(const char *file_name, int dir_position, whodata_evt *evt, 
                     opts & CHECK_MTIME ? '+' : '-',
                     opts & CHECK_INODE ? '+' : '-',
                     opts & CHECK_SHA256SUM ? '+' : '-',
+                    opts & CHECK_ATTRS ? '+' : '-',
                     opts & CHECK_SEECHANGES ? '+' : '-',
                     opts & CHECK_SIZE ? (long)statbuf.st_size : 0,
                     opts & CHECK_PERM ? (int)statbuf.st_mode : 0,
@@ -316,9 +317,10 @@ static int read_file(const char *file_name, int dir_position, whodata_evt *evt, 
                     opts & CHECK_GROUP ? get_group(statbuf.st_gid) : "",
                     opts & CHECK_MTIME ? (long)statbuf.st_mtime : 0,
                     opts & CHECK_INODE ? (long)statbuf.st_ino : 0,
-                    opts & CHECK_SHA256SUM ? sf256_sum : "");
+                    opts & CHECK_SHA256SUM ? sf256_sum : "",
+                    opts & CHECK_ATTRS ? get_attrs(file_name) : 0);
 #else
-            snprintf(alert_msg, OS_MAXSTR, "%c%c%c%c%c%c%c%c%c%c%ld:%d:%d:%d:%s:%s:%s:%s:%ld:%ld:%s",
+            snprintf(alert_msg, OS_MAXSTR, "%c%c%c%c%c%c%c%c%c%c%c%ld:%d:%d:%d:%s:%s:%s:%s:%ld:%ld:%s:%u",
                     opts & CHECK_SIZE ? '+' : '-',
                     opts & CHECK_PERM ? '+' : '-',
                     opts & CHECK_OWNER ? '+' : '-',
@@ -328,6 +330,7 @@ static int read_file(const char *file_name, int dir_position, whodata_evt *evt, 
                     opts & CHECK_MTIME ? '+' : '-',
                     opts & CHECK_INODE ? '+' : '-',
                     opts & CHECK_SHA256SUM ? '+' : '-',
+                    opts & CHECK_ATTRS ? '+' : '-',
                     opts & CHECK_SEECHANGES ? '+' : '-',
                     opts & CHECK_SIZE ? (long)statbuf.st_size : 0,
                     opts & CHECK_PERM ? (int)statbuf.st_mode : 0,
@@ -339,7 +342,8 @@ static int read_file(const char *file_name, int dir_position, whodata_evt *evt, 
                     opts & CHECK_GROUP ? get_group(statbuf.st_gid) : "",
                     opts & CHECK_MTIME ? (long)statbuf.st_mtime : 0,
                     opts & CHECK_INODE ? (long)statbuf.st_ino : 0,
-                    opts & CHECK_SHA256SUM ? sf256_sum : "");
+                    opts & CHECK_SHA256SUM ? sf256_sum : "",
+                    0);
 #endif
 
             os_calloc(1, sizeof(syscheck_node), s_node);
@@ -361,7 +365,7 @@ static int read_file(const char *file_name, int dir_position, whodata_evt *evt, 
             }
 
 #ifdef WIN32
-            snprintf(alert_msg, OS_MAXSTR, "%ld:%d:%s::%s:%s:%s:%s:%ld:%ld:%s!%s:%s %s%s%s",
+            snprintf(alert_msg, OS_MAXSTR, "%ld:%d:%s::%s:%s:%s:%s:%ld:%ld:%s:%u!%s:%s %s%s%s",
                 opts & CHECK_SIZE ? (long)statbuf.st_size : 0,
                 opts & CHECK_PERM ? (int)statbuf.st_mode : 0,
                 (opts & CHECK_OWNER) && sid ? sid : "",
@@ -372,13 +376,14 @@ static int read_file(const char *file_name, int dir_position, whodata_evt *evt, 
                 opts & CHECK_MTIME ? (long)statbuf.st_mtime : 0,
                 opts & CHECK_INODE ? (long)statbuf.st_ino : 0,
                 opts & CHECK_SHA256SUM ? sf256_sum : "",
+                opts & CHECK_ATTRS ? get_attrs(file_name) : 0,
                 wd_sum,
                 syscheck.tag[dir_position] ? syscheck.tag[dir_position] : "",
                 file_name,
                 alertdump ? "\n" : "",
                 alertdump ? alertdump : "");
 #else
-            snprintf(alert_msg, OS_MAXSTR, "%ld:%d:%d:%d:%s:%s:%s:%s:%ld:%ld:%s!%s:%s %s%s%s",
+            snprintf(alert_msg, OS_MAXSTR, "%ld:%d:%d:%d:%s:%s:%s:%s:%ld:%ld:%s:%u!%s:%s %s%s%s",
                 opts & CHECK_SIZE ? (long)statbuf.st_size : 0,
                 opts & CHECK_PERM ? (int)statbuf.st_mode : 0,
                 opts & CHECK_OWNER ? (int)statbuf.st_uid : 0,
@@ -390,6 +395,7 @@ static int read_file(const char *file_name, int dir_position, whodata_evt *evt, 
                 opts & CHECK_MTIME ? (long)statbuf.st_mtime : 0,
                 opts & CHECK_INODE ? (long)statbuf.st_ino : 0,
                 opts & CHECK_SHA256SUM ? sf256_sum : "",
+                0,
                 wd_sum,
                 syscheck.tag[dir_position] ? syscheck.tag[dir_position] : "",
                 file_name,

src/syscheckd/run_check.c
@@ -334,6 +334,7 @@ int c_read_file(const char *file_name, const char *oldsum, char *newsum, whodata
     os_sha1 sf_sum;
     os_sha256 sf256_sum;
     syscheck_node *s_node;
+    unsigned int attributes = 0;
 #ifdef WIN32
     char *sid = NULL;
     const char *user;
@@ -426,8 +427,15 @@ int c_read_file(const char *file_name, const char *oldsum, char *newsum, whodata
         sha256sum = 1;
     }
 
+    /* Attributes*/
+#ifdef WIN32
+    if (oldsum[9] == '+') {
+        attributes = get_attrs(file_name);
+    }
+#endif
+
     /* Report changes */
-    if (oldsum[9] == '-') {
+    if (oldsum[10] == '-') {
         delete_target_file(file_name);
     }
 
@@ -459,11 +467,11 @@ int c_read_file(const char *file_name, const char *oldsum, char *newsum, whodata
                         strncpy(mf_sum, "n/a", 4);
                         strncpy(sf256_sum, "n/a", 4);
                     }
-                }
+                } 
             }
         }
     }
-    snprintf(newsum, OS_MAXSTR, "%ld:%d:%d:%d:%s:%s:%s:%s:%ld:%ld:%s",
+    snprintf(newsum, OS_MAXSTR, "%ld:%d:%d:%d:%s:%s:%s:%s:%ld:%ld:%s:%u",
         size == 0 ? 0 : (long)statbuf.st_size,
         perm == 0 ? 0 : (int)statbuf.st_mode,
         owner == 0 ? 0 : (int)statbuf.st_uid,
@@ -474,11 +482,12 @@ int c_read_file(const char *file_name, const char *oldsum, char *newsum, whodata
         group == 0 ? "" : get_group(statbuf.st_gid),
         mtime ? (long)statbuf.st_mtime : 0,
         inode ? (long)statbuf.st_ino : 0,
-        sha256sum  == 0 ? "" : sf256_sum);
+        sha256sum  == 0 ? "" : sf256_sum,
+        attributes);
 #else
     user = get_user(file_name, statbuf.st_uid, &sid);
 
-    snprintf(newsum, OS_MAXSTR, "%ld:%d:%s::%s:%s:%s:%s:%ld:%ld:%s",
+    snprintf(newsum, OS_MAXSTR, "%ld:%d:%s::%s:%s:%s:%s:%ld:%ld:%s:%u",
         size == 0 ? 0 : (long)statbuf.st_size,
         perm == 0 ? 0 : (int)statbuf.st_mode,
         (owner == 0) && sid ? "" : sid,
@@ -488,7 +497,8 @@ int c_read_file(const char *file_name, const char *oldsum, char *newsum, whodata
         group == 0 ? "" : get_group(statbuf.st_gid),
         mtime ? (long)statbuf.st_mtime : 0,
         inode ? (long)statbuf.st_ino : 0,
-        sha256sum  == 0 ? "" : sf256_sum);
+        sha256sum  == 0 ? "" : sf256_sum,
+        attributes);
 
         if (sid) {
             LocalFree(sid);

wazuh/wazuh-agent

src/headers/syscheck_op.h
@@ -20,6 +20,26 @@
 #include <grp.h>
 #define PATH_SEP '/'
 
+#define FILE_ATTRIBUTE_READONLY                0x00000001
+#define FILE_ATTRIBUTE_HIDDEN                  0x00000002
+#define FILE_ATTRIBUTE_SYSTEM                  0x00000004
+#define FILE_ATTRIBUTE_DIRECTORY               0x00000010
+#define FILE_ATTRIBUTE_ARCHIVE                 0x00000020
+#define FILE_ATTRIBUTE_DEVICE                  0x00000040
+#define FILE_ATTRIBUTE_NORMAL                  0x00000080
+#define FILE_ATTRIBUTE_TEMPORARY               0x00000100
+#define FILE_ATTRIBUTE_SPARSE_FILE             0x00000200
+#define FILE_ATTRIBUTE_REPARSE_POINT           0x00000400
+#define FILE_ATTRIBUTE_COMPRESSED              0x00000800
+#define FILE_ATTRIBUTE_OFFLINE                 0x00001000
+#define FILE_ATTRIBUTE_NOT_CONTENT_INDEXED     0x00002000
+#define FILE_ATTRIBUTE_ENCRYPTED               0x00004000
+#define FILE_ATTRIBUTE_INTEGRITY_STREAM        0x00008000
+#define FILE_ATTRIBUTE_VIRTUAL                 0x00010000
+#define FILE_ATTRIBUTE_NO_SCRUB_DATA           0x00020000
+#define FILE_ATTRIBUTE_RECALL_ON_OPEN          0x00040000
+#define FILE_ATTRIBUTE_RECALL_ON_DATA_ACCESS   0x00400000
+
 #else
 
 #include "shared.h"
@@ -43,6 +63,7 @@ typedef enum sk_syscheck {
     SK_GNAME,
     SK_INODE,
     SK_SHA256,
+    SK_ATTRS,
     SK_MTIME,
     SK_CHFIELDS,
     SK_USER_ID,
@@ -71,6 +92,7 @@ typedef struct __sdb {
     char sha256[OS_FLSIZE + 1];
     char mtime[OS_FLSIZE + 1];
     char inode[OS_FLSIZE + 1];
+    char attrs[OS_SIZE_1024 + 1];
 
     // Whodata fields
     char user_id[OS_FLSIZE + 1];
@@ -112,6 +134,7 @@ typedef struct sk_sum_t {
     char *md5;
     char *sha1;
     char *sha256;
+    unsigned int attrs;
     char *uname;
     char *gname;
     long mtime;
@@ -150,10 +173,13 @@ void normalize_path(char *path);
 
 const char *get_user(__attribute__((unused)) const char *path, int uid, __attribute__((unused)) char **sid);
 const char* get_group(int gid);
+void get_attributes_str(char *str, unsigned int attrs);
 
 #else
 
 const char *get_user(const char *path, __attribute__((unused)) int uid, char **sid);
+unsigned int w_get_attrs(const char *file_path);
+int w_get_permissions(const char *file_path, char *permissions);
 const char *get_group(__attribute__((unused)) int gid);
 
 #endif

src/shared/syscheck_op.c
@@ -22,6 +22,7 @@ int sk_decode_sum(sk_sum_t *sum, char *c_sum, char *w_sum) {
     char *c_perm;
     char *c_mtime;
     char *c_inode;
+    char *attrs;
     char *tag;
     int retval = 0;
 
@@ -86,6 +87,10 @@ int sk_decode_sum(sk_sum_t *sum, char *c_sum, char *w_sum) {
                     if (tag = strchr(sum->sha256, ':'), tag) {
                         *(tag++) = '\0';
                         sum->tag = tag;
+                        if ((attrs = strchr(sum->sha256, ':'))) {
+                            *(attrs++) = '\0';
+                            sum->attrs = strtoul(attrs, NULL, 10);
+                        }
                     }
                 }
 
@@ -505,6 +510,35 @@ const char* get_group(int gid) {
     return group ? group->gr_name : "";
 }
 
+void get_attributes_str(char *str, unsigned int attrs) {
+    size_t size;
+
+    size = snprintf(str, OS_SIZE_256, "%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s",
+                    attrs & FILE_ATTRIBUTE_ARCHIVE ? "ARCHIVE ," : "",
+                    attrs & FILE_ATTRIBUTE_COMPRESSED ? "COMPRESSED ," : "",
+                    attrs & FILE_ATTRIBUTE_DEVICE ? "DEVICE ," : "",
+                    attrs & FILE_ATTRIBUTE_DIRECTORY ? "DIRECTORY ," : "",
+                    attrs & FILE_ATTRIBUTE_ENCRYPTED ? "ENCRYPTED ," : "",
+                    attrs & FILE_ATTRIBUTE_HIDDEN ? "HIDDEN ," : "",
+                    attrs & FILE_ATTRIBUTE_INTEGRITY_STREAM ? "INTEGRITY_STREAM ," : "",
+                    attrs & FILE_ATTRIBUTE_NORMAL ? "NORMAL ," : "",
+                    attrs & FILE_ATTRIBUTE_NOT_CONTENT_INDEXED ? "NOT_CONTENT_INDEXED ," : "",
+                    attrs & FILE_ATTRIBUTE_NO_SCRUB_DATA ? "NO_SCRUB_DATA ," : "",
+                    attrs & FILE_ATTRIBUTE_OFFLINE ? "OFFLINE ," : "",
+                    attrs & FILE_ATTRIBUTE_READONLY ? "READONLY ," : "",
+                    attrs & FILE_ATTRIBUTE_RECALL_ON_DATA_ACCESS ? "RECALL_ON_DATA_ACCESS ," : "",
+                    attrs & FILE_ATTRIBUTE_RECALL_ON_OPEN ? "RECALL_ON_OPEN ," : "",
+                    attrs & FILE_ATTRIBUTE_REPARSE_POINT ? "REPARSE_POINT ," : "",
+                    attrs & FILE_ATTRIBUTE_SPARSE_FILE ? "SPARSE_FILE ," : "",
+                    attrs & FILE_ATTRIBUTE_SYSTEM ? "SYSTEM ," : "",
+                    attrs & FILE_ATTRIBUTE_TEMPORARY ? "TEMPORARY ," : "",
+                    attrs & FILE_ATTRIBUTE_VIRTUAL ? "VIRTUAL ," : ""
+    );
+    if (size > 1) {
+        str[size - 2] = '\0';
+    }
+}
+
 #else
 
 const char *get_user(const char *path, __attribute__((unused)) int uid, char **sid)
@@ -614,6 +648,25 @@ end:
         LocalFree(pSD);
     }
     return AcctName;
+}
+
+int w_get_permissions(const char *file_path, char *permissions) {
+  int retval = 0;
+  *permissions = '\0';
+  SECURITY_DESCRIPTOR *s_desc;
+
+  return retval;
+}
+
+unsigned int w_get_attrs(const char *file_path) {
+    unsigned int attrs;
+
+    if (attrs = GetFileAttributesA(file_path), attrs == INVALID_FILE_ATTRIBUTES) {
+        attrs = 0;
+        merror("The attributes for '%s' could not be obtained. Error '%ld'.", file_path, GetLastError());
+    }
+
+    return attrs;
 }
 
 const char *get_group(__attribute__((unused)) int gid) {

src/syscheckd/create_db.c
@@ -187,6 +187,7 @@ static int read_file(const char *file_name, int dir_position, whodata_evt *evt, 
 #ifdef WIN32
     const char *user = NULL;
     char *sid = NULL;
+    char permissions[OS_SIZE_20480 + 1];
 #else
     char str_owner[50], str_group[50];
 #endif
@@ -305,6 +306,15 @@ static int read_file(const char *file_name, int dir_position, whodata_evt *evt, 
         if (opts & CHECK_OWNER) {
             user = get_user(file_name, statbuf.st_uid, &sid);
         }
+
+        // Get the file permissions
+        *permissions = '\0';
+        if (opts & CHECK_PERM) {
+          int error;
+          if (error = w_get_permissions(file_name, permissions), error) {
+            merror("No se pudieron extraer permisos de la '%s'. Error: %d.", file_name, error);
+          }
+        }
 #endif
 
         if (s_node = (syscheck_node *) OSHash_Get_ex(syscheck.fp, file_name), !s_node) {
@@ -340,7 +350,7 @@ static int read_file(const char *file_name, int dir_position, whodata_evt *evt, 
                 *str_inode = '\0';
             }
 
-            snprintf(alert_msg, OS_MAXSTR, "%c%c%c%c%c%c%c%c%c%c%s:%s:%s::%s:%s:%s:%s:%s:%s:%s",
+            snprintf(alert_msg, OS_MAXSTR, "%c%c%c%c%c%c%c%c%c%c%c%s:%s:%s::%s:%s:%s:%s:%s:%s:%s:%u",
                     opts & CHECK_SIZE ? '+' : '-',
                     opts & CHECK_PERM ? '+' : '-',
                     opts & CHECK_OWNER ? '+' : '-',
@@ -350,9 +360,10 @@ static int read_file(const char *file_name, int dir_position, whodata_evt *evt, 
                     opts & CHECK_MTIME ? '+' : '-',
                     opts & CHECK_INODE ? '+' : '-',
                     opts & CHECK_SHA256SUM ? '+' : '-',
+                    opts & CHECK_ATTRS ? '+' : '-',
                     opts & CHECK_SEECHANGES ? '+' : '-',
                     str_size,
-                    str_perm,
+                    permissions,
                     (opts & CHECK_OWNER) && sid ? sid : "",
                     opts & CHECK_MD5SUM ? mf_sum : "",
                     opts & CHECK_SHA1SUM ? sf_sum : "",
@@ -360,7 +371,8 @@ static int read_file(const char *file_name, int dir_position, whodata_evt *evt, 
                     opts & CHECK_GROUP ? get_group(statbuf.st_gid) : "",
                     str_mtime,
                     str_inode,
-                    opts & CHECK_SHA256SUM ? sf256_sum : "");
+                    opts & CHECK_SHA256SUM ? sf256_sum : "",
+                    opts & CHECK_ATTRS ? w_get_attrs(file_name) : 0);
 #else
             if (opts & CHECK_SIZE) {
                 sprintf(str_size,"%ld",(long)statbuf.st_size);
@@ -398,7 +410,7 @@ static int read_file(const char *file_name, int dir_position, whodata_evt *evt, 
                 *str_inode = '\0';
             }
 
-            snprintf(alert_msg, OS_MAXSTR, "%c%c%c%c%c%c%c%c%c%c%s:%s:%s:%s:%s:%s:%s:%s:%s:%s:%s",
+            snprintf(alert_msg, OS_MAXSTR, "%c%c%c%c%c%c%c%c%c%c%c%s:%s:%s:%s:%s:%s:%s:%s:%s:%s:%s:%u",
                     opts & CHECK_SIZE ? '+' : '-',
                     opts & CHECK_PERM ? '+' : '-',
                     opts & CHECK_OWNER ? '+' : '-',
@@ -408,6 +420,7 @@ static int read_file(const char *file_name, int dir_position, whodata_evt *evt, 
                     opts & CHECK_MTIME ? '+' : '-',
                     opts & CHECK_INODE ? '+' : '-',
                     opts & CHECK_SHA256SUM ? '+' : '-',
+                    opts & CHECK_ATTRS ? '+' : '-',
                     opts & CHECK_SEECHANGES ? '+' : '-',
                     str_size,
                     str_perm,
@@ -419,7 +432,8 @@ static int read_file(const char *file_name, int dir_position, whodata_evt *evt, 
                     opts & CHECK_GROUP ? get_group(statbuf.st_gid) : "",
                     str_mtime,
                     str_inode,
-                    opts & CHECK_SHA256SUM ? sf256_sum : "");
+                    opts & CHECK_SHA256SUM ? sf256_sum : "",
+                    0);
 #endif
 
             os_calloc(1, sizeof(syscheck_node), s_node);
@@ -465,9 +479,9 @@ static int read_file(const char *file_name, int dir_position, whodata_evt *evt, 
                 *str_inode = '\0';
             }
 
-            snprintf(alert_msg, OS_MAXSTR, "%s:%s:%s::%s:%s:%s:%s:%s:%s:%s!%s:%s %s%s%s",
+            snprintf(alert_msg, OS_MAXSTR, "%s:%s:%s::%s:%s:%s:%s:%s:%s:%s:%u!%s:%s %s%s%s",
                 str_size,
-                str_perm,
+                permissions,
                 (opts & CHECK_OWNER) && sid ? sid : "",
                 opts & CHECK_MD5SUM ? mf_sum : "",
                 opts & CHECK_SHA1SUM ? sf_sum : "",
@@ -476,6 +490,7 @@ static int read_file(const char *file_name, int dir_position, whodata_evt *evt, 
                 str_mtime,
                 str_inode,
                 opts & CHECK_SHA256SUM ? sf256_sum : "",
+                opts & CHECK_ATTRS ? w_get_attrs(file_name) : 0,
                 wd_sum,
                 syscheck.tag[dir_position] ? syscheck.tag[dir_position] : "",
                 file_name,
@@ -518,9 +533,9 @@ static int read_file(const char *file_name, int dir_position, whodata_evt *evt, 
                 *str_inode = '\0';
             }
 
-            snprintf(alert_msg, OS_MAXSTR, "%s:%s:%s:%s:%s:%s:%s:%s:%s:%s:%s!%s:%s %s%s%s",
+            snprintf(alert_msg, OS_MAXSTR, "%s:%s:%s:%s:%s:%s:%s:%s:%s:%s:%s:%u!%s:%s %s%s%s",
                 str_size,
-                str_perm,
+                permissions,
                 str_owner,
                 str_group,
                 opts & CHECK_MD5SUM ? mf_sum : "",
@@ -530,6 +545,7 @@ static int read_file(const char *file_name, int dir_position, whodata_evt *evt, 
                 str_mtime,
                 str_inode,
                 opts & CHECK_SHA256SUM ? sf256_sum : "",
+                0,
                 wd_sum,
                 syscheck.tag[dir_position] ? syscheck.tag[dir_position] : "",
                 file_name,

src/syscheckd/run_check.c
@@ -314,11 +314,12 @@ int c_read_file(const char *file_name, const char *oldsum, char *newsum, whodata
     os_sha256 sf256_sum;
     syscheck_node *s_node;
     char str_size[50], str_perm[50], str_mtime[50], str_inode[50];
-#ifndef WIN32
-    char str_owner[50], str_group[50];
-#else
+#ifdef WIN32
+    unsigned int attributes = 0;
     char *sid = NULL;
     const char *user;
+#else
+    char str_owner[50], str_group[50];
 #endif
 
     /* Clean sums */
@@ -408,8 +409,15 @@ int c_read_file(const char *file_name, const char *oldsum, char *newsum, whodata
         sha256sum = 1;
     }
 
+    /* Attributes*/
+#ifdef WIN32
+    if (oldsum[9] == '+') {
+        attributes = w_get_attrs(file_name);
+    }
+#endif
+
     /* Report changes */
-    if (oldsum[9] == '-') {
+    if (oldsum[10] == '-') {
         delete_target_file(file_name);
     }
 
@@ -482,7 +490,7 @@ int c_read_file(const char *file_name, const char *oldsum, char *newsum, whodata
         sprintf(str_inode, "%ld", (long)statbuf.st_ino);
     }
 
-    snprintf(newsum, OS_MAXSTR, "%s:%s:%s:%s:%s:%s:%s:%s:%s:%s:%s",
+    snprintf(newsum, OS_MAXSTR, "%s:%s:%s:%s:%s:%s:%s:%s:%s:%s:%s:%u",
         str_size,
         str_perm,
         str_owner,
@@ -493,7 +501,8 @@ int c_read_file(const char *file_name, const char *oldsum, char *newsum, whodata
         group == 0 ? "" : get_group(statbuf.st_gid),
         str_mtime,
         str_inode,
-        sha256sum  == 0 ? "" : sf256_sum);
+        sha256sum  == 0 ? "" : sf256_sum,
+        0);
 #else
     user = get_user(file_name, statbuf.st_uid, &sid);
 
@@ -521,7 +530,7 @@ int c_read_file(const char *file_name, const char *oldsum, char *newsum, whodata
         sprintf(str_inode, "%ld", (long)statbuf.st_ino);
     }
 
-    snprintf(newsum, OS_MAXSTR, "%s:%s:%s::%s:%s:%s:%s:%s:%s:%s",
+    snprintf(newsum, OS_MAXSTR, "%s:%s:%s::%s:%s:%s:%s:%s:%s:%s:%u",
         str_size,
         str_perm,
         (owner == 0) && sid ? "" : sid,
@@ -531,7 +540,8 @@ int c_read_file(const char *file_name, const char *oldsum, char *newsum, whodata
         group == 0 ? "" : get_group(statbuf.st_gid),
         str_mtime,
         str_inode,
-        sha256sum  == 0 ? "" : sf256_sum);
+        sha256sum  == 0 ? "" : sf256_sum,
+        attributes);
 
         if (sid) {
             LocalFree(sid);