romansad/wazuh

Add endless loop to deal with other possible scenarios

src/syscheckd/fim_db.c
@@ -240,13 +240,13 @@ void fim_db_bind_get_path_inode(fdb_t *fim_sql, const char *file_path);
 
 fdb_t *fim_db_init(int storage) {
     fdb_t *fim;
-    char *path = (storage == 1) ? FIM_DB_MEMORY_PATH : FIM_DB_DISK_PATH;
+    char *path = (storage == FIM_DB_MEMORY) ? FIM_DB_MEMORY_PATH : FIM_DB_DISK_PATH;
 
     os_calloc(1, sizeof(fdb_t), fim);
     fim->transaction.interval = COMMIT_INTERVAL;
 
-    if (fim_db_clean() < 0) {
-        goto free_fim;
+    if (storage == FIM_DB_DISK) {
+        fim_db_clean();
     }
 
     if (fim_db_create_file(path, schema_fim_sql, storage, &fim->db) < 0) {
@@ -294,24 +294,34 @@ void fim_db_close(fdb_t *fim_sql) {
 }
 
 
-int fim_db_clean(void) {
-    int rm = FIMDB_OK;
+void fim_db_clean(void) {
 
     if (w_is_file(FIM_DB_DISK_PATH)) {
         // If the file is being used by other processes, wait until
-        // it's unlocked in order to remove it.
-        int i;
-        for (i = 1; i <= 5 && (rm = remove(FIM_DB_DISK_PATH)); i++) {
-            mdebug2("SQL Error: Failed to access '%s' - %dº try.", FIM_DB_DISK_PATH, i);
+        // it's unlocked in order to remove it. Wait at most 5 seconds.
+        int i, rm;
+        for (i = 1; i <= FIMDB_RM_MAX_LOOP && (rm = remove(FIM_DB_DISK_PATH)); i++) {
+            mdebug2("Failed to delete '%s' - %dº try.", FIM_DB_DISK_PATH, i);
 #ifdef WIN32
-            Sleep(200*i);
+            Sleep(FIMDB_RM_DEFAULT_TIME*i); //miliseconds
 #else
-            usleep(100*i);
+            usleep(FIMDB_RM_DEFAULT_TIME*i); //miliseconds
 #endif
+        }
+
+        //Loop endlessly until the file can be removed.
+        if (rm == FIMDB_ERR) {
+            while (remove(FIM_DB_DISK_PATH)) {
+                mdebug2("Failed to delete '%s'", FIM_DB_DISK_PATH);
+#ifdef WIN32
+                Sleep(60000); //miliseconds
+#else
+                sleep(60); //seconds
+#endif
+            }
         }
     }
 
-    return rm;
 }
 
 

wazuh/wazuh-agent

src/syscheckd/fim_db.c
@@ -240,13 +240,13 @@ void fim_db_bind_get_path_inode(fdb_t *fim_sql, const char *file_path);
 
 fdb_t *fim_db_init(int storage) {
     fdb_t *fim;
-    char *path = (storage == 1) ? FIM_DB_MEMORY_PATH : FIM_DB_DISK_PATH;
+    char *path = (storage == FIM_DB_MEMORY) ? FIM_DB_MEMORY_PATH : FIM_DB_DISK_PATH;
 
     os_calloc(1, sizeof(fdb_t), fim);
     fim->transaction.interval = COMMIT_INTERVAL;
 
-    if (fim_db_clean() < 0) {
-        goto free_fim;
+    if (storage == FIM_DB_DISK) {
+        fim_db_clean();
     }
 
     if (fim_db_create_file(path, schema_fim_sql, storage, &fim->db) < 0) {
@@ -294,24 +294,34 @@ void fim_db_close(fdb_t *fim_sql) {
 }
 
 
-int fim_db_clean(void) {
-    int rm = FIMDB_OK;
+void fim_db_clean(void) {
 
     if (w_is_file(FIM_DB_DISK_PATH)) {
         // If the file is being used by other processes, wait until
-        // it's unlocked in order to remove it.
-        int i;
-        for (i = 1; i <= 5 && (rm = remove(FIM_DB_DISK_PATH)); i++) {
-            mdebug2("SQL Error: Failed to access '%s' - %dº try.", FIM_DB_DISK_PATH, i);
+        // it's unlocked in order to remove it. Wait at most 5 seconds.
+        int i, rm;
+        for (i = 1; i <= FIMDB_RM_MAX_LOOP && (rm = remove(FIM_DB_DISK_PATH)); i++) {
+            mdebug2("Failed to delete '%s' - %dº try.", FIM_DB_DISK_PATH, i);
 #ifdef WIN32
-            Sleep(200*i);
+            Sleep(FIMDB_RM_DEFAULT_TIME*i); //milliseconds
 #else
-            usleep(100*i);
+            usleep(FIMDB_RM_DEFAULT_TIME*i); //milliseconds
 #endif
+        }
+
+        //Loop endlessly until the file can be removed.
+        if (rm == FIMDB_ERR) {
+            while (remove(FIM_DB_DISK_PATH)) {
+                mdebug2("Failed to delete '%s'", FIM_DB_DISK_PATH);
+#ifdef WIN32
+                Sleep(60000); //milliseconds
+#else
+                sleep(60); //seconds
+#endif
+            }
         }
     }
 
-    return rm;
 }
 
 