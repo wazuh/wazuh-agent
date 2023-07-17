caowei-sea/wazuh

Add snap package manager support to data provider (#15740)
* fix http-request make library and add to project only by Linux

* fix install_time format and UT also

src/data_provider/src/packages/packageLinuxParserHelper.h
@@ -185,8 +185,13 @@ namespace PackageLinuxHelper
         }
 
         if (info.contains("install-date"))
-        {
-            install_time = info.at("install-date");
+        {   
+            struct std::tm tm;
+            std::istringstream ss(static_cast<std::string>(info.at("install-date")));
+            ss >> std::get_time(&tm, "%Y-%m-%dT%H:%M:%S");
+            std::ostringstream oss;
+            oss << std::put_time(&tm, "%Y/%m/%d %H:%M:%S");
+            install_time = oss.str();
         }
 
         if (info.contains("summary"))

wazuh/wazuh-agent

src/data_provider/src/packages/packageLinuxParserHelper.h
@@ -186,7 +186,12 @@ namespace PackageLinuxHelper
 
         if (info.contains("install-date"))
         {
-            install_time = info.at("install-date");
+            struct std::tm tm;
+            std::istringstream iss(static_cast<std::string>(info.at("install-date")));
+            iss >> std::get_time(&tm, "%Y-%m-%dT%H:%M:%S");
+            std::ostringstream oss;
+            oss << std::put_time(&tm, "%Y/%m/%d %H:%M:%S");
+            install_time = oss.str();
         }
 
         if (info.contains("summary"))