b798461f61aa6d16bbb7a35a1aa6c73a8c74eea4
Nicolas Gomez Palacios <nicogp2@gmail.com>
2024-07-12 02:52:34 -0300

    feat(configuration-parser): added the initial structure of the class and some tests

---
+++ src/agent/configuration_parser/include/configuration_parser.hpp
@@ -0,0 +1,36 @@
+#pragma once
+
+#include <toml.hpp>
+
+#include <iostream>
+
+namespace configuration
+{
+    class ConfigurationParser
+    {
+    private:
+        std::string configFileName = "wazuh.conf";
+        toml::value tbl;
+
+    public:
+        ConfigurationParser();
+        ConfigurationParser(std::string stringToParse);
+        ~ConfigurationParser() {};
+
+        template<typename T, typename ... Ks>
+        auto GetConfig(Ks ... ks)
+        {
+            try
+            {
+                auto config = toml::find<T>(tbl, ks...);
+                return config;
+            }
+            catch(const std::exception& e)
+            {
+                std::cout << "The requested value could not be obtained: " << e.what() << std::endl;
+                throw;
+            }
+        };
+
+    };
+} // namespace configuration
---
+++ src/agent/configuration_parser/src/configuration_parser.cpp
@@ -0,0 +1,31 @@
+#include <configuration_parser.hpp>
+
+namespace configuration
+{
+    ConfigurationParser::ConfigurationParser()
+    {
+        try
+        {
+            tbl = toml::parse(configFileName, toml::spec::v(1,0,0));
+        }
+        catch(const std::exception& e)
+        {
+            std::cout << "Error parsing wazuh.conf file: " << e.what() << std::endl;
+            throw;
+        }
+    }
+
+    ConfigurationParser::ConfigurationParser(std::string stringToParse)
+    {
+        try
+        {
+            tbl = toml::parse_str(stringToParse, toml::spec::v(1,0,0));
+        }
+        catch(const std::exception& e)
+        {
+            std::cout << "Error parsing wazuh.conf file: " << e.what() << std::endl;
+            throw;
+        }
+    }
+
+} // configuration
