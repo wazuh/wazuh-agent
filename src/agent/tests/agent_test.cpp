005062b2a7608976d0aec2cb241390e8693faa99
jr0me <joaquin.romera@wazuh.com>
2024-07-17 02:49:20 -0300

    feat: add google test as dependency with example test

--- src/agent/tests/agent_test.cpp
+++ src/agent/tests/agent_test.cpp
@@ -1,7 +1,14 @@
 #include <agent.hpp>
 
+#include <gtest/gtest.h>
+
+TEST(AgentTests, AgentConstruction)
+{
+    EXPECT_NO_THROW(Agent {});
+}
+
 int main(int argc, char** argv)
 {
-    Agent agent;
-    return 0;
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
 }
