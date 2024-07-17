960cbb84eef58f96ab25eda1bbb1a1e31697301c
jr0me <joaquin.romera@wazuh.com>
2024-07-17 15:15:55 -0300

    feat: add task manager and queue message tasks in agent

--- src/agent/src/agent.cpp
+++ src/agent/src/agent.cpp
@@ -1,5 +1,20 @@
 #include "agent.hpp"
 
-Agent::Agent() {}
+#include <message_task.hpp>
 
-Agent::~Agent() {}
+#include <chrono>
+#include <thread>
+
+Agent::Agent()
+{
+    m_taskManager.start(std::thread::hardware_concurrency());
+    m_taskManager.enqueueTask(StatefulMessageProcessingTask(m_messageQueue));
+    m_taskManager.enqueueTask(StatelessMessageProcessingTask(m_messageQueue));
+}
+
+Agent::~Agent()
+{
+    // Sleep for 2 seconds
+    std::this_thread::sleep_for(std::chrono::seconds(2));
+    m_taskManager.stop();
+}
