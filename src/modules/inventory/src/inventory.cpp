7a0ae9ddab5b726eb66aa1031956055293b64e05
ncvicchi <norberto.vicchi@wazuh.com>
2024-07-17 16:01:20 -0300

    feat: moving functions from inventoryimpl&syscollector to inventory class

--- src/modules/inventory/src/inventory.cpp
+++ src/modules/inventory/src/inventory.cpp
@@ -22,8 +22,95 @@ const int DBSYNC_MQ    = '5';
 #define WM_INV_LOGTAG "modules:inventory" // Tag for log messages
 #define WM_INVENTORY_DEFAULT_INTERVAL W_HOUR_SECONDS
 
+void Inventory::Inventory(){
+    inv_stop_condition = PTHREAD_COND_INITIALIZER;
+    inv_stop_mutex = PTHREAD_MUTEX_INITIALIZER;
+    need_shutdown_wait = false;
+    inv_reconnect_mutex = PTHREAD_MUTEX_INITIALIZER;
+    shutdown_process_started = false;
+
+    dbPath = INVENTORY_DB_DISK_PATH;
+    normalizerConfigPath = INVENTORY_NORM_CONFIG_DISK_PATH;
+    normalizerType = INVENTORY_NORM_TYPE;
+}
+
+void Inventory::~Inventory(){
+    cout << "Inventory destroyed!" << endl;
+}
+
 void *Inventory::run() {
-    cout << "+ [Inventory] is running" << endl;
+    w_cond_init(&inv_stop_condition, NULL);
+    w_mutex_init(&inv_stop_mutex, NULL);
+    w_mutex_init(&inv_reconnect_mutex, NULL);
+
+    if (!enabled) {
+        mtinfo(WM_INV_LOGTAG, "Module disabled. Exiting...");
+        pthread_exit(NULL);
+    }
+
+    #ifndef WIN32
+    // Connect to socket
+    queue_fd = StartMQ(DEFAULTQUEUE, WRITE, INFINITE_OPENQ_ATTEMPTS);
+
+    if (queue_fd < 0) {
+        mterror(WM_INV_LOGTAG, "Can't connect to queue.");
+        pthread_exit(NULL);
+    }
+    #endif
+
+    if (inventory_module = so_get_module_handle("inventory"), inventory_module)
+    {
+        inventory_sync_message_ptr = so_get_function_sym(inventory_module, "inventory_sync_message");
+
+        void* rsync_module = NULL;
+        if(rsync_module = so_check_module_loaded("rsync"), rsync_module) {
+            rsync_initialize_full_log_func rsync_initialize_log_function_ptr = so_get_function_sym(rsync_module, "rsync_initialize_full_log_function");
+            if(rsync_initialize_log_function_ptr) {
+                rsync_initialize_log_function_ptr(mtLoggingFunctionsWrapper);
+            }
+            // Even when the RTLD_NOLOAD flag was used for dlopen(), we need a matching call to dlclose()
+#ifndef WIN32
+            so_free_library(rsync_module);
+#endif
+        }
+    } else {
+#ifdef __hpux
+        mtinfo(WM_INV_LOGTAG, "Not supported in HP-UX.");
+#else
+        mterror(WM_INV_LOGTAG, "Can't load inventory.");
+#endif
+        pthread_exit(NULL);
+    }
+
+    mtdebug1(WM_INV_LOGTAG, "Starting inventory.");
+    w_mutex_lock(&inv_stop_mutex);
+    need_shutdown_wait = true;
+    w_mutex_unlock(&inv_stop_mutex);
+    const long max_eps = inv->sync.sync_max_eps;
+    if (0 != max_eps) {
+        inventory_sync_max_eps = max_eps;
+    }
+    // else: if max_eps is 0 (from configuration) let's use the default max_eps value (10)
+    wm_inv_log_config(inv);
+
+    inventory_start();
+
+    inventory_sync_message_ptr = NULL;
+    inventory_start_ptr = NULL;
+    inventory_stop_ptr = NULL;
+
+    if (queue_fd) {
+        close(queue_fd);
+        queue_fd = 0;
+    }
+    so_free_library(inventory_module);
+
+    inventory_module = NULL;
+    mtinfo(WM_INV_LOGTAG, "Module finished.");
+    w_mutex_lock(&inv_stop_mutex);
+    w_cond_signal(&inv_stop_condition);
+    w_mutex_unlock(&inv_stop_mutex);
+    return 0;
 
     return NULL;
 }
@@ -49,6 +136,7 @@ int Inventory::setup(const Configuration & config) {
 
 void Inventory::stop() {
     cout << "- [Inventory] stopped" << endl;
+    Inventory::instance().destroy();
 }
 
 string Inventory::command(const string & query) {
@@ -129,4 +217,83 @@ cJSON * Inventory::wm_inv_dump() {
     cJSON_AddItemToObject(root,"inventory",wm_inv);
 
     return root;
+}
+
+
+void Inventory::inventory_start()
+{
+    std::function<void(const std::string&)> callbackDiffWrapper
+    {
+        [callbackDiff](const std::string & data)
+        {
+            callbackDiff(data.c_str());
+        }
+    };
+
+    std::function<void(const std::string&)> callbackSyncWrapper
+    {
+        [callbackSync](const std::string & data)
+        {
+            callbackSync(data.c_str());
+        }
+    };
+
+    std::function<void(const modules_log_level_t, const std::string&)> callbackLogWrapper
+    {
+        [callbackLog](const modules_log_level_t level, const std::string & data)
+        {
+            callbackLog(level, data.c_str(), WM_INV_LOGTAG);
+        }
+    };
+
+    std::function<void(const std::string&)> callbackErrorLogWrapper
+    {
+        [callbackLog](const std::string & data)
+        {
+            callbackLog(LOG_ERROR, data.c_str(), WM_INV_LOGTAG);
+        }
+    };
+
+    DBSync::initialize(callbackErrorLogWrapper);
+
+    try
+    {
+        Inventory::instance().init(std::make_shared<SysInfo>(),
+                                      std::move(callbackDiffWrapper),
+                                      std::move(callbackSyncWrapper),
+                                      std::move(callbackLogWrapper),
+                                      dbPath,
+                                      normalizerConfigPath,
+                                      normalizerType,
+                                      inverval,
+                                      scanOnStart,
+                                      hardware,
+                                      os,
+                                      network,
+                                      packages,
+                                      ports,
+                                      portsAll,
+                                      processes,
+                                      hotfixes);
+    }
+    catch (const std::exception& ex)
+    {
+        callbackErrorLogWrapper(ex.what());
+    }
+}
+
+int Inventory::inventory_sync_message(const char* data)
+{
+    int ret{-1};
+
+    try
+    {
+        Inventory::instance().push(data);
+        ret = 0;
+    }
+    catch (...)
+    {
+    }
+
+    return ret;
 }
