0657ee039709aae57871e850fba816c58296b5b9
Santiago Vendramini <sdvendramini@gmail.com>
2024-07-30 13:34:16 -0300

    feat: added register functionality
    The logic to parse arguments in the main and a handler to process the registration of an agent are added.
    For this, register.cpp and register.hpp files are created where only the handler is exposed.
    Then a namespace is added to define the functions to perform the registration itself and the
    authentication and registration requests to the Server Management API.

--- src/agent/src/main.cpp
+++ src/agent/src/main.cpp
@@ -1,8 +1,53 @@
 #include "agent.hpp"
-#include <iostream>
+#include "cmd_ln_parser.hpp"
+#include "register.hpp"
 
-int main()
+#include <iostream>
+#include <optional>
+
+int main(int argc, char* argv[])
 {
+    CommandlineParser cmdParser(argc, argv);
+
+    if (cmdParser.OptionExists("--register"))
+    {
+        std::cout << "Starting registration process" << std::endl;
+
+        if (cmdParser.OptionExists("--user") && cmdParser.OptionExists("--password"))
+        {
+            const auto user = cmdParser.getOptionValue("--user");
+            const auto password = cmdParser.getOptionValue("--password");
+            std::optional<std::string> name;
+            std::optional<std::string> ip;
+
+            if (cmdParser.OptionExists("--name"))
+            {
+                name = cmdParser.getOptionValue("--name");
+            }
+
+            if (cmdParser.OptionExists("--ip"))
+            {
+                ip = cmdParser.getOptionValue("--ip");
+            }
+
+            if (RegisterAgent(user, password, name, ip))
+            {
+                std::cout << "Agent registered." << std::endl;
+            }
+            else
+            {
+                std::cout << "Registration fail." << std::endl;
+            }
+        }
+        else
+        {
+            std::cout << "--user and --password args are mandatory" << std::endl;
+        }
+
+        std::cout << "Exiting ..." << std::endl;
+        return 0;
+    }
+
     Agent agent;
     std::this_thread::sleep_for(std::chrono::seconds(15));
 }
