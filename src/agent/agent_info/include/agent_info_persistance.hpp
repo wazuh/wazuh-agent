ef9e37711666dc2724b935d85bf37c287cc71596
jr0me <joaquin.romera@wazuh.com>
2024-07-31 23:02:40 +0200

    feat: add AgentInfoPersistance class together with basic test

--- src/agent/agent_info/include/agent_info_persistance.hpp
+++ src/agent/agent_info/include/agent_info_persistance.hpp
@@ -1,1 +1,28 @@
 #pragma once
+
+#include <memory>
+#include <string>
+
+static constexpr char sqlitedb_path[] = "agent_info.db";
+
+namespace SQLite
+{
+    class Database;
+}
+
+class AgentInfoPersistance
+{
+public:
+    explicit AgentInfoPersistance(const std::string& db_path = sqlitedb_path);
+    ~AgentInfoPersistance();
+
+    AgentInfoPersistance(const AgentInfoPersistance&) = delete;
+    AgentInfoPersistance& operator=(const AgentInfoPersistance&) = delete;
+    AgentInfoPersistance(AgentInfoPersistance&&) = delete;
+    AgentInfoPersistance& operator=(AgentInfoPersistance&&) = delete;
+
+private:
+    void CreateAgentInfoTable();
+
+    std::unique_ptr<SQLite::Database> m_db;
+};
--- src/agent/agent_info/tests/agent_info_persistance_test.cpp
+++ src/agent/agent_info/tests/agent_info_persistance_test.cpp
@@ -2,6 +2,25 @@
 
 #include <agent_info_persistance.hpp>
 
+#include <memory>
+#include <string>
+
+class AgentInfoPersistanceTest : public ::testing::Test
+{
+protected:
+    void SetUp() override
+    {
+        persistance = std::make_unique<AgentInfoPersistance>("agent_info_test.db");
+    }
+
+    std::unique_ptr<AgentInfoPersistance> persistance;
+};
+
+TEST_F(AgentInfoPersistanceTest, TestConstruction)
+{
+    EXPECT_NE(persistance, nullptr);
+}
+
 int main(int argc, char** argv)
 {
     ::testing::InitGoogleTest(&argc, argv);
