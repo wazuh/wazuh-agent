838b0ca0fff0c66658e5bac602e777ea0d72d57b
jr0me <joaquin.romera@wazuh.com>
2024-07-31 22:48:58 +0200

    feat: add AgentInfo boilerplate code and files

---
+++ src/agent/agent_info/tests/agent_info_persistance_test.cpp
@@ -0,0 +1,9 @@
+#include <gtest/gtest.h>
+
+#include <agent_info_persistance.hpp>
+
+int main(int argc, char** argv)
+{
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
---
+++ src/agent/agent_info/tests/agent_info_test.cpp
@@ -0,0 +1,9 @@
+#include <gtest/gtest.h>
+
+#include <agent_info.hpp>
+
+int main(int argc, char** argv)
+{
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
