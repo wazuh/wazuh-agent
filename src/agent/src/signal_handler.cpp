43d5aeaed38b91b618a296204d1d0758cd5a7b2c
jr0me <joaquin.romera@wazuh.com>
2024-08-05 17:58:00 +0200

    feat: add mechanism to make SignalHandler wait for signals
    It will block while waiting.

--- src/agent/src/signal_handler.cpp
+++ src/agent/src/signal_handler.cpp
@@ -1,6 +1,8 @@
 #include <signal_handler.hpp>
 
 std::atomic<bool> SignalHandler::KeepRunning = true;
+std::condition_variable SignalHandler::m_cv;
+std::mutex SignalHandler::m_cvMutex;
 
 SignalHandler::SignalHandler(const std::vector<int>& signalsToHandle)
 {
@@ -13,4 +15,11 @@ SignalHandler::SignalHandler(const std::vector<int>& signalsToHandle)
 void SignalHandler::HandleSignal(int signal)
 {
     KeepRunning.store(false);
+    m_cv.notify_one();
+}
+
+void SignalHandler::WaitForSignal()
+{
+    std::unique_lock<std::mutex> lock(m_cvMutex);
+    m_cv.wait(lock, [] { return !KeepRunning.load(); });
 }
