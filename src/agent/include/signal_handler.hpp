120f63468b7409db38320e5e32de636c6a58e691
jr0me <joaquin.romera@wazuh.com>
2024-08-05 17:57:08 +0200

    feat: SignalHandler handles SIGINT and SIGTERM

--- src/agent/include/signal_handler.hpp
+++ src/agent/include/signal_handler.hpp
@@ -1,5 +1,15 @@
 #pragma once
 
+#include <atomic>
+#include <csignal>
+#include <vector>
+
 class SignalHandler
 {
+public:
+    SignalHandler(const std::vector<int>& signalsToHandle = {SIGINT, SIGTERM});
+
+    static void HandleSignal(int signalToHandle);
+
+    static std::atomic<bool> KeepRunning;
 };
--- src/agent/src/signal_handler.cpp
+++ src/agent/src/signal_handler.cpp
@@ -1,1 +1,16 @@
 #include <signal_handler.hpp>
+
+std::atomic<bool> SignalHandler::KeepRunning = true;
+
+SignalHandler::SignalHandler(const std::vector<int>& signalsToHandle)
+{
+    for (const auto signal : signalsToHandle)
+    {
+        std::signal(signal, SignalHandler::HandleSignal);
+    }
+}
+
+void SignalHandler::HandleSignal(int signal)
+{
+    KeepRunning.store(false);
+}
