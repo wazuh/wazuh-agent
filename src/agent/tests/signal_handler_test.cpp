aa186a107cde7f75d6e9c593bd43a44e22207cf1
jr0me <joaquin.romera@wazuh.com>
2024-08-05 17:57:08 +0200

    feat: SignalHandler handles SIGINT and SIGTERM

--- src/agent/tests/signal_handler_test.cpp
+++ src/agent/tests/signal_handler_test.cpp
@@ -2,6 +2,24 @@
 
 #include <signal_handler.hpp>
 
+class SignalHandlerTest : public ::testing::Test
+{
+protected:
+    void SetUp() override
+    {
+        SignalHandler::KeepRunning.store(true);
+    }
+
+    void TearDown() override {}
+
+    SignalHandler signalHandler = SignalHandler({SIGUSR1});
+};
+
+TEST_F(SignalHandlerTest, KeepRunningIsTheDefault)
+{
+    EXPECT_TRUE(SignalHandler::KeepRunning.load());
+}
+
 int main(int argc, char** argv)
 {
     ::testing::InitGoogleTest(&argc, argv);
