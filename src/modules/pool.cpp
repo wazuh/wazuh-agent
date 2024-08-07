3bc45d1eb83003d211d5e5e26c6e8f168a431ea8
nbertoldo <nicobertoldo@gmail.com>
2024-08-07 19:08:59 -0300

    refactor: Modified class Pool to allow singleton instances

--- src/modules/pool.cpp
+++ src/modules/pool.cpp
@@ -4,21 +4,21 @@
 Pool::Pool() {
     // addModule(make_shared<LogCollector>());
     // addModule(make_shared<FIM>());
-    addModule(make_shared<Inventory>());
+    addModule(Inventory::instance());
     // addModule(make_shared<SCA>());
 }
 
-template <Module T>
-void Pool::addModule(shared_ptr<T> module) {
+template <typename T>
+void Pool::addModule(T& module) {
     auto wrapper = make_shared<ModuleWrapper>(ModuleWrapper{
-        .run = [module]() { module->run(); },
-        .setup = [module](const Configuration & config) { return module->setup(config); },
-        .stop = [module]() { module->stop(); },
-        .command = [module](const string & query) { return module->command(query); },
-        .name = [module]() { return module->name(); }
+        .run = [&module]() { module.run(); },
+        .setup = [&module](const Configuration & config) { return module.setup(config); },
+        .stop = [&module]() { module.stop(); },
+        .command = [&module](const string & query) { return module.command(query); },
+        .name = [&module]() { return module.name(); }
     });
 
-    modules[module->name()] = wrapper;
+    modules[module.name()] = wrapper;
 }
 
 shared_ptr<ModuleWrapper> Pool::getModule(const string & name) {
@@ -43,9 +43,3 @@ void Pool::stop() {
         module->stop();
     }
 }
-
-// Explicit template instantiation
-// template void Pool::addModule(shared_ptr<LogCollector> module);
-// template void Pool::addModule(shared_ptr<FIM> module);
-template void Pool::addModule(shared_ptr<Inventory> module);
-// template void Pool::addModule(shared_ptr<SCA> module);
