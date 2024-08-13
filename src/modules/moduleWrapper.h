3ab3d1c0eedb93ed2252750a6045a3f758eeeea5
nbertoldo <nicobertoldo@gmail.com>
2024-08-13 19:11:02 -0300

    refactor: added a thread for each module in the pool class

--- src/modules/moduleWrapper.h
+++ src/modules/moduleWrapper.h
@@ -8,15 +8,11 @@
 using namespace std;
 
 struct ModuleWrapper {
-    function<void()> run;                              // Main function
+    function<void()> run;
     function<int(const Configuration&)> setup;
-    function<void(void *)> destroy;                     // Configuration destructor
-    function<void()> stop;                              // Module destructor
+    function<void()> stop;
     function<string(const string&)> command;
-    function<size_t(void *, char *, char **)> query;    // Run a query
     function<string()> name;
-    function<string(int(const string&))> sync;          // Sync
-    function<string(const void *)>dump;                 // Dump current configuration
 };
 
 #endif // MODULE_WRAPPER_H
