2e1a93bcbb56782f87a6c397d68f74020c8f29b2
jr0me <joaquin.romera@wazuh.com>
2024-08-22 13:26:15 +0200

    feat: create coverage target
    This way we can pass the option COVERAGE to the cmake
    configuration step. Then with 'make coverage' the
    report will be generated.
    
    We found that GCC and GCOV fail to report line coverage
    with coroutines. For this reason we try to use llvm-cov
    if available.

--- src/cmake/CommonSettings.cmake
+++ src/cmake/CommonSettings.cmake
@@ -3,6 +3,37 @@ function(set_common_settings)
     set(CMAKE_CXX_STANDARD_REQUIRED ON PARENT_SCOPE)
     set(CMAKE_EXPORT_COMPILE_COMMANDS ON PARENT_SCOPE)
 
+    if(COVERAGE)
+        if(NOT TARGET coverage)
+            set(CMAKE_BUILD_TYPE Debug)
+
+            if(CMAKE_CXX_COMPILER_ID MATCHES "Clang")
+                add_compile_options(-O0 -fprofile-arcs -ftest-coverage -fprofile-instr-generate -fcoverage-mapping)
+                add_link_options(-fprofile-arcs -ftest-coverage -fprofile-instr-generate -fcoverage-mapping)
+            else()
+                add_compile_options(-O0 --coverage) # note that -g3 is required for gcov but it's already below
+                add_link_options(--coverage)
+            endif()
+
+            # We found that gcc gcov doesn't report coverage on
+            # coroutines, so we use llvm-cov if available
+            find_program(LLVM_COV_EXECUTABLE NAMES llvm-cov)
+
+            if(LLVM_COV_EXECUTABLE)
+                set(GCOV_EXECUTABLE "llvm-cov gcov")
+            else()
+                set(GCOV_EXECUTABLE "gcov")
+            endif()
+
+            add_custom_target(coverage
+                COMMAND ${CMAKE_MAKE_PROGRAM} test
+                COMMAND gcovr --gcov-executable "${GCOV_EXECUTABLE}" -v -r .. -e '.*main\.cpp' -e '.*vcpkg.*' -e '.*mock_.*' -e '.*_test.*' --html --html-details -o coverage.html
+                WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
+                COMMENT "Generating code coverage report"
+            )
+        endif()
+    endif()
+
     if(NOT CMAKE_BUILD_TYPE)
         set(CMAKE_BUILD_TYPE "RelWithDebInfo" PARENT_SCOPE)
     endif()
