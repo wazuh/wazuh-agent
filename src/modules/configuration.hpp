d9dbc646f1b1d1dccdeb001fa2c6a6ad8a5e67b6
nbertoldo <nicobertoldo@gmail.com>
2024-08-22 16:09:24 -0300

    feat: Added inventory configuration functions

--- src/modules/configuration.hpp
+++ src/modules/configuration.hpp
@@ -1,6 +1,38 @@
 #ifndef CONFIGURATION_H
 #define CONFIGURATION_H
 
-class Configuration { };
+#include "stringHelper.h"
+
+#define INVENTORY_DEFAULT_INTERVAL W_HOUR_SECONDS
+
+struct InventoryConfig {
+    bool enabled = true;
+    unsigned int interval = INVENTORY_DEFAULT_INTERVAL;
+    bool scanOnStart = true;
+    bool hardware = true;
+    bool os = true;
+    bool network = true;
+    bool packages = true;
+    bool ports = true;
+    bool portsAll = true;
+    bool processes = true;
+    bool hotfixes = true;
+};
+
+class Configuration {
+    public:
+        Configuration() = default;
+
+        Configuration(const InventoryConfig& config) : inventoryConfig(config) {}
+
+        void setInventoryConfig(const InventoryConfig& config) {
+            inventoryConfig = config;
+        }
+
+        const InventoryConfig& getInventoryConfig() const { return inventoryConfig; }
+
+    private:
+        InventoryConfig inventoryConfig;
+};
 
 #endif // CONFIGURATION_H
