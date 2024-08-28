6ae42d71b417cc12d77c0cbb830ba6c2a058ba26
jr0me <joaquin.romera@wazuh.com>
2024-08-28 18:59:44 +0200

    feat: test communicator failed authentication

--- src/agent/communicator/tests/communicator_test.cpp
+++ src/agent/communicator/tests/communicator_test.cpp
@@ -49,6 +49,63 @@ TEST(CommunicatorTest, StatefulMessageProcessingTask_Success)
     ioContext.run();
 }
 
+TEST(CommunicatorTest, WaitForTokenExpirationAndAuthenticate_FailedAuthenticationLeavesEmptyToken)
+{
+    auto mockHttpClient = std::make_unique<MockHttpClient>();
+    auto mockHttpClientPtr = mockHttpClient.get();
+
+    // not really a leak, as its lifetime is managed by the Communicator
+    testing::Mock::AllowLeak(mockHttpClientPtr);
+
+    auto communicatorPtr =
+        std::make_shared<communicator::Communicator>(std::move(mockHttpClient), "uuid", "key", nullptr);
+
+    // A failed authentication won't return a token
+    EXPECT_CALL(*mockHttpClientPtr, AuthenticateWithUuidAndKey(_, _, _, _))
+        .WillOnce(Invoke(
+            [communicatorPtr](const std::string& host,
+                              const std::string& port,
+                              const std::string& uuid,
+                              const std::string& key) -> std::optional<std::string>
+            {
+                communicatorPtr->Stop();
+                return std::nullopt;
+            }));
+
+    // A following call to Co_PerformHttpRequest should not have a token
+    EXPECT_CALL(*mockHttpClientPtr, Co_PerformHttpRequest(_, _, _, _, _, _))
+        .WillOnce(Invoke(
+            [](const std::string& token,
+               http_client::HttpRequestParams,
+               std::function<boost::asio::awaitable<std::string>()> getMessages,
+               std::function<void()> onUnauthorized,
+               std::function<void(const std::string&)> onSuccess,
+               std::function<bool()> loopCondition) -> boost::asio::awaitable<void>
+            {
+                EXPECT_TRUE(token.empty());
+                co_return;
+            }));
+
+    boost::asio::io_context ioContext;
+
+    boost::asio::co_spawn(
+        ioContext,
+        [communicatorPtr]() mutable -> boost::asio::awaitable<void>
+        { co_await communicatorPtr->WaitForTokenExpirationAndAuthenticate(); },
+        boost::asio::detached);
+
+    boost::asio::co_spawn(
+        ioContext,
+        [communicatorPtr]() mutable -> boost::asio::awaitable<void>
+        {
+            co_await communicatorPtr->StatelessMessageProcessingTask(
+                []() -> boost::asio::awaitable<std::string> { co_return "message"; }, [](const std::string& msg) {});
+        },
+        boost::asio::detached);
+
+    ioContext.run();
+}
+
 int main(int argc, char** argv)
 {
     ::testing::InitGoogleTest(&argc, argv);
