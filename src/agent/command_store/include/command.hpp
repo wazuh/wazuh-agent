c6145777a9315ff80471b9fe09752a51f4533e21
Ariel Martin <ariel.martin@wazuh.com>
2024-09-05 14:31:31 -0300

    feat: added Command class

---
+++ src/agent/command_store/include/command.hpp
@@ -0,0 +1,44 @@
+#pragma once
+
+#include <limits>
+#include <string>
+
+namespace command_store
+{
+    enum class Status
+    {
+        SUCCESS,
+        ERROR,
+        IN_PROGRESS,
+        TIMEOUT,
+        UNKNOWN
+    };
+
+    class Command
+    {
+    public:
+        Command()
+            : m_id(std::numeric_limits<int>::min())
+            , m_status(Status::UNKNOWN)
+            , m_time(0.0)
+        {
+        }
+
+        Command(
+            int id, const std::string& module, const std::string& command, const std::string& parameters, Status status)
+            : m_id(id)
+            , m_module(module)
+            , m_command(command)
+            , m_parameters(parameters)
+            , m_status(status)
+        {
+        }
+
+        int m_id;
+        std::string m_module;
+        std::string m_command;
+        std::string m_parameters;
+        Status m_status;
+        double m_time;
+    };
+} // namespace command_store
