zbalkan/wazuh-agent-1

feat: added Update() function and Command::m_result member

src/agent/command_store/src/command_store.cpp
@@ -26,13 +26,14 @@ namespace command_store
         sqlite_manager::Column colModule {"module", sqlite_manager::ColumnType::TEXT, true, false, false};
         sqlite_manager::Column colCommand {"command", sqlite_manager::ColumnType::TEXT, true, false, false};
         sqlite_manager::Column colParameter {"parameter", sqlite_manager::ColumnType::TEXT, true, false, false};
+        sqlite_manager::Column colResult {"result", sqlite_manager::ColumnType::TEXT, true, false, false};
         sqlite_manager::Column colStatus {"status", sqlite_manager::ColumnType::INTEGER, true, false, false};
         sqlite_manager::Column colTime {"time", sqlite_manager::ColumnType::REAL, true, false, false};
 
         try
         {
             m_dataBase->CreateTable(COMMANDSTORE_TABLE_NAME,
-                                    {colId, colModule, colCommand, colParameter, colStatus, colTime});
+                                    {colId, colModule, colCommand, colParameter, colResult, colStatus, colTime});
         }
         catch (std::exception& e)
         {
@@ -87,6 +88,7 @@ namespace command_store
         fields.emplace_back("command", sqlite_manager::ColumnType::TEXT, cmd.m_command);
         fields.emplace_back("time", sqlite_manager::ColumnType::REAL, std::to_string(GetCurrentTimestampAsReal()));
         fields.emplace_back("parameter", sqlite_manager::ColumnType::TEXT, cmd.m_parameters);
+        fields.emplace_back("result", sqlite_manager::ColumnType::TEXT, cmd.m_result);
         fields.emplace_back(
             "status", sqlite_manager::ColumnType::INTEGER, std::to_string(static_cast<int>(cmd.m_status)));
         try
@@ -144,9 +146,13 @@ namespace command_store
             {
                 cmd.m_command = col.m_value;
             }
-            else if (col.m_name == "parameters")
+            else if (col.m_name == "parameter")
             {
                 cmd.m_parameters = col.m_value;
+            }
+            else if (col.m_name == "result")
+            {
+                cmd.m_result = col.m_value;
             }
             else if (col.m_name == "status")
             {
@@ -158,5 +164,33 @@ namespace command_store
             }
         }
         return cmd;
+    }
+
+    bool CommandStore::UpdateCommand(const Command& cmd)
+    {
+        std::vector<sqlite_manager::Column> fields;
+        if (!cmd.m_module.empty())
+            fields.emplace_back("module", sqlite_manager::ColumnType::TEXT, cmd.m_module);
+        if (!cmd.m_command.empty())
+            fields.emplace_back("command", sqlite_manager::ColumnType::TEXT, cmd.m_command);
+        if (!cmd.m_parameters.empty())
+            fields.emplace_back("parameter", sqlite_manager::ColumnType::TEXT, cmd.m_parameters);
+        if (!cmd.m_result.empty())
+            fields.emplace_back("result", sqlite_manager::ColumnType::TEXT, cmd.m_result);
+        if (cmd.m_status != Status::UNKNOWN)
+            fields.emplace_back(
+                "status", sqlite_manager::ColumnType::INTEGER, std::to_string(static_cast<int>(cmd.m_status)));
+
+        sqlite_manager::Column condition("id", sqlite_manager::ColumnType::INTEGER, std::to_string(cmd.m_id));
+        try
+        {
+            m_dataBase->Update(COMMANDSTORE_TABLE_NAME, fields, {condition});
+        }
+        catch (const std::exception& e)
+        {
+            std::cerr << "UpdateCommand operation failed. " << e.what() << "\n";
+            return false;
+        }
+        return true;
     }
 } // namespace command_store

src/agent/command_store/tests/command_store_test.cpp
@@ -2,6 +2,10 @@
 
 #include <command_store.hpp>
 
+constexpr int FIVE = 5;
+constexpr int NINE = 9;
+constexpr int ELEVEN = 11;
+
 class CommandStoreTest : public ::testing::Test
 {
 protected:
@@ -20,15 +24,90 @@ TEST_F(CommandStoreTest, StoreCommandTest)
     m_commandStore->Clear();
 
     // NOLINTNEXTLINE(cppcoreguidelines-avoid-magic-numbers)
-    command_store::Command cmd1(5, "Module1", "{CommandTextHERE}", "Parameter1", command_store::Status::IN_PROGRESS);
+    command_store::Command cmd1(
+        FIVE, "Module1", "{CommandTextHERE}", "Parameter1", "Result1", command_store::Status::IN_PROGRESS);
+    bool retVal = m_commandStore->StoreCommand(cmd1);
+
+    // NOLINTNEXTLINE(cppcoreguidelines-avoid-magic-numbers)
+    command_store::Command cmd2(
+        NINE, "Module2", R"({"Some"="thing"})", "Parameter2", "Result2", command_store::Status::IN_PROGRESS);
+    retVal = m_commandStore->StoreCommand(cmd2);
+    ASSERT_EQ(retVal, true);
+
+    // NOLINTNEXTLINE(cppcoreguidelines-avoid-magic-numbers)
+    command_store::Command cmd3(
+        FIVE, "Module3", "{CommandTextHERE}", "Parameter3", "Result3", command_store::Status::IN_PROGRESS);
+    retVal = m_commandStore->StoreCommand(cmd3);
+    ASSERT_EQ(retVal, false);
+
+    ASSERT_EQ(m_commandStore->GetCount(), 2);
+}
+
+TEST_F(CommandStoreTest, UpdateCommandTest)
+{
+    m_commandStore->Clear();
+
+    // NOLINTNEXTLINE(cppcoreguidelines-avoid-magic-numbers)
+    command_store::Command cmd1(
+        FIVE, "Module1", "{CommandTextHERE}", "Parameter1", "Result1", command_store::Status::IN_PROGRESS);
     m_commandStore->StoreCommand(cmd1);
     // NOLINTNEXTLINE(cppcoreguidelines-avoid-magic-numbers)
-    command_store::Command cmd2(9, "Module2", R"({"Some"="thing"})", "Parameter2", command_store::Status::IN_PROGRESS);
+    command_store::Command cmd2(
+        NINE, "Module2", R"({"Some"="thing"})", "Parameter2", "Result2", command_store::Status::IN_PROGRESS);
     m_commandStore->StoreCommand(cmd2);
-    // NOLINTNEXTLINE(cppcoreguidelines-avoid-magic-numbers)
-    command_store::Command cmd3(5, "Module3", "{CommandTextHERE}", "Parameter3", command_store::Status::IN_PROGRESS);
-    m_commandStore->StoreCommand(cmd3);
+
     ASSERT_EQ(m_commandStore->GetCount(), 2);
+
+    // NOLINTNEXTLINE(cppcoreguidelines-avoid-magic-numbers)
+    command_store::Command cmdUpdate(NINE,
+                                     "Updated Module",
+                                     "Updated Command",
+                                     "Updated Parameter",
+                                     "Updated Result",
+                                     command_store::Status::SUCCESS);
+
+    bool retVal = m_commandStore->UpdateCommand(cmdUpdate);
+    ASSERT_EQ(retVal, true);
+
+    // NOLINTNEXTLINE(cppcoreguidelines-avoid-magic-numbers)
+    std::optional<command_store::Command> retValue = m_commandStore->GetCommand(9);
+    if (retValue.has_value())
+    {
+        const command_store::Command& cmd = retValue.value();
+        ASSERT_EQ(cmd.m_id, NINE);
+        ASSERT_EQ(cmd.m_module, "Updated Module");
+        ASSERT_EQ(cmd.m_command, "Updated Command");
+        ASSERT_EQ(cmd.m_parameters, "Updated Parameter");
+        ASSERT_EQ(cmd.m_result, "Updated Result");
+        ASSERT_EQ(cmd.m_status, command_store::Status::SUCCESS);
+    }
+    else
+    {
+        FAIL();
+    }
+
+    // NOLINTNEXTLINE(cppcoreguidelines-avoid-magic-numbers)
+    command_store::Command cmdUpdate2(NINE, "", "", "", "Newly Updated Result", command_store::Status::UNKNOWN);
+
+    retVal = m_commandStore->UpdateCommand(cmdUpdate2);
+    ASSERT_EQ(retVal, true);
+
+    // NOLINTNEXTLINE(cppcoreguidelines-avoid-magic-numbers)
+    retValue = m_commandStore->GetCommand(NINE);
+    if (retValue.has_value())
+    {
+        const command_store::Command& cmd = retValue.value();
+        ASSERT_EQ(cmd.m_id, NINE);
+        ASSERT_EQ(cmd.m_module, "Updated Module");
+        ASSERT_EQ(cmd.m_command, "Updated Command");
+        ASSERT_EQ(cmd.m_parameters, "Updated Parameter");
+        ASSERT_EQ(cmd.m_result, "Newly Updated Result");
+        ASSERT_EQ(cmd.m_status, command_store::Status::SUCCESS);
+    }
+    else
+    {
+        FAIL();
+    }
 }
 
 TEST_F(CommandStoreTest, DeleteCommandTest)
@@ -36,7 +115,7 @@ TEST_F(CommandStoreTest, DeleteCommandTest)
     int initialCount = m_commandStore->GetCount();
 
     // NOLINTNEXTLINE(cppcoreguidelines-avoid-magic-numbers)
-    m_commandStore->DeleteCommand(9);
+    m_commandStore->DeleteCommand(NINE);
 
     ASSERT_EQ(m_commandStore->GetCount(), initialCount - 1);
 }
@@ -46,13 +125,16 @@ TEST_F(CommandStoreTest, GetCommandTest)
     m_commandStore->Clear();
 
     // NOLINTNEXTLINE(cppcoreguidelines-avoid-magic-numbers)
-    command_store::Command cmd1(5, "Module1", "{CommandTextHERE}", "Parameter1", command_store::Status::IN_PROGRESS);
+    command_store::Command cmd1(
+        FIVE, "Module1", "{CommandTextHERE}", "Parameter1", "Result1", command_store::Status::IN_PROGRESS);
     m_commandStore->StoreCommand(cmd1);
     // NOLINTNEXTLINE(cppcoreguidelines-avoid-magic-numbers)
-    command_store::Command cmd2(9, "Module2", "TestValue9", "Parameter2", command_store::Status::IN_PROGRESS);
+    command_store::Command cmd2(
+        NINE, "Module2", "TestValue9", "Parameter2", "Result2", command_store::Status::IN_PROGRESS);
     m_commandStore->StoreCommand(cmd2);
     // NOLINTNEXTLINE(cppcoreguidelines-avoid-magic-numbers)
-    command_store::Command cmd3(11, "Module3", "{CommandTextHERE}", "Parameter3", command_store::Status::IN_PROGRESS);
+    command_store::Command cmd3(
+        ELEVEN, "Module3", "{CommandTextHERE}", "Parameter3", "Result3", command_store::Status::IN_PROGRESS);
     m_commandStore->StoreCommand(cmd3);
     ASSERT_EQ(m_commandStore->GetCount(), 3);
 
@@ -61,17 +143,17 @@ TEST_F(CommandStoreTest, GetCommandTest)
     if (retValue.has_value())
     {
         const command_store::Command& cmd = retValue.value();
-        ASSERT_EQ(cmd.m_id, 9);
+        ASSERT_EQ(cmd.m_id, NINE);
         ASSERT_EQ(cmd.m_module, "Module2");
         ASSERT_EQ(cmd.m_command, "TestValue9");
     }
 
     // NOLINTNEXTLINE(cppcoreguidelines-avoid-magic-numbers)
-    retValue = m_commandStore->GetCommand(11);
+    retValue = m_commandStore->GetCommand(ELEVEN);
     if (retValue.has_value())
     {
         const command_store::Command& cmd = retValue.value();
-        ASSERT_EQ(cmd.m_id, 11);
+        ASSERT_EQ(cmd.m_id, ELEVEN);
         ASSERT_EQ(cmd.m_module, "Module3");
         ASSERT_EQ(cmd.m_command, "{CommandTextHERE}");
     }

wazuh/wazuh-agent

src/agent/command_store/src/command_store.cpp
@@ -26,13 +26,14 @@ namespace command_store
         sqlite_manager::Column colModule {"module", sqlite_manager::ColumnType::TEXT, true, false, false};
         sqlite_manager::Column colCommand {"command", sqlite_manager::ColumnType::TEXT, true, false, false};
         sqlite_manager::Column colParameter {"parameters", sqlite_manager::ColumnType::TEXT, true, false, false};
+        sqlite_manager::Column colResult {"result", sqlite_manager::ColumnType::TEXT, true, false, false};
         sqlite_manager::Column colStatus {"status", sqlite_manager::ColumnType::INTEGER, true, false, false};
         sqlite_manager::Column colTime {"time", sqlite_manager::ColumnType::REAL, true, false, false};
 
         try
         {
             m_dataBase->CreateTable(COMMANDSTORE_TABLE_NAME,
-                                    {colId, colModule, colCommand, colParameter, colStatus, colTime});
+                                    {colId, colModule, colCommand, colParameter, colResult, colStatus, colTime});
         }
         catch (std::exception& e)
         {
@@ -87,6 +88,7 @@ namespace command_store
         fields.emplace_back("command", sqlite_manager::ColumnType::TEXT, cmd.m_command);
         fields.emplace_back("time", sqlite_manager::ColumnType::REAL, std::to_string(GetCurrentTimestampAsReal()));
         fields.emplace_back("parameters", sqlite_manager::ColumnType::TEXT, cmd.m_parameters);
+        fields.emplace_back("result", sqlite_manager::ColumnType::TEXT, cmd.m_result);
         fields.emplace_back(
             "status", sqlite_manager::ColumnType::INTEGER, std::to_string(static_cast<int>(cmd.m_status)));
         try
@@ -119,44 +121,84 @@ namespace command_store
 
     std::optional<Command> CommandStore::GetCommand(int id)
     {
-        auto cmdData =
-            m_dataBase->Select(COMMANDSTORE_TABLE_NAME,
-                               {},
-                               {sqlite_manager::Column("id", sqlite_manager::ColumnType::INTEGER, std::to_string(id))});
-
-        if (cmdData.empty())
+        try
         {
+            auto cmdData = m_dataBase->Select(
+                COMMANDSTORE_TABLE_NAME,
+                {},
+                {sqlite_manager::Column("id", sqlite_manager::ColumnType::INTEGER, std::to_string(id))});
+
+            if (cmdData.empty())
+            {
+                return std::nullopt;
+            }
+
+            Command cmd;
+            for (const sqlite_manager::Column& col : cmdData[0])
+            {
+                if (col.m_name == "id")
+                {
+                    cmd.m_id = std::stoi(col.m_value);
+                }
+                else if (col.m_name == "module")
+                {
+                    cmd.m_module = col.m_value;
+                }
+                else if (col.m_name == "command")
+                {
+                    cmd.m_command = col.m_value;
+                }
+                else if (col.m_name == "parameters")
+                {
+                    cmd.m_parameters = col.m_value;
+                }
+                else if (col.m_name == "result")
+                {
+                    cmd.m_result = col.m_value;
+                }
+                else if (col.m_name == "status")
+                {
+                    cmd.m_status = StatusFromInt(std::stoi(col.m_value));
+                }
+                else if (col.m_name == "time")
+                {
+                    cmd.m_time = std::stod(col.m_value);
+                }
+            }
+            return cmd;
+        }
+        catch (const std::exception& e)
+        {
+            std::cerr << "Select operation failed. " << e.what() << "\n";
             return std::nullopt;
         }
+    }
 
-        Command cmd;
-        for (const sqlite_manager::Column& col : cmdData[0])
+    bool CommandStore::UpdateCommand(const Command& cmd)
+    {
+        std::vector<sqlite_manager::Column> fields;
+        if (!cmd.m_module.empty())
+            fields.emplace_back("module", sqlite_manager::ColumnType::TEXT, cmd.m_module);
+        if (!cmd.m_command.empty())
+            fields.emplace_back("command", sqlite_manager::ColumnType::TEXT, cmd.m_command);
+        if (!cmd.m_parameters.empty())
+            fields.emplace_back("parameters", sqlite_manager::ColumnType::TEXT, cmd.m_parameters);
+        if (!cmd.m_result.empty())
+            fields.emplace_back("result", sqlite_manager::ColumnType::TEXT, cmd.m_result);
+        if (cmd.m_status != Status::UNKNOWN)
+            fields.emplace_back(
+                "status", sqlite_manager::ColumnType::INTEGER, std::to_string(static_cast<int>(cmd.m_status)));
+
+        sqlite_manager::Column condition("id", sqlite_manager::ColumnType::INTEGER, std::to_string(cmd.m_id));
+        try
         {
-            if (col.m_name == "id")
-            {
-                cmd.m_id = std::stoi(col.m_value);
-            }
-            else if (col.m_name == "module")
-            {
-                cmd.m_module = col.m_value;
-            }
-            else if (col.m_name == "command")
-            {
-                cmd.m_command = col.m_value;
-            }
-            else if (col.m_name == "parameters")
-            {
-                cmd.m_parameters = col.m_value;
-            }
-            else if (col.m_name == "status")
-            {
-                cmd.m_status = StatusFromInt(std::stoi(col.m_value));
-            }
-            else if (col.m_name == "time")
-            {
-                cmd.m_time = std::stod(col.m_value);
-            }
+            m_dataBase->Update(COMMANDSTORE_TABLE_NAME, fields, {condition});
         }
-        return cmd;
+        catch (const std::exception& e)
+        {
+            std::cerr << "UpdateCommand operation failed. " << e.what() << "\n";
+            return false;
+        }
+        return true;
     }
 } // namespace command_store

src/agent/command_store/tests/command_store_test.cpp
@@ -2,6 +2,10 @@
 
 #include <command_store.hpp>
 
+constexpr int TESTID_5 = 5;
+constexpr int TESTID_9 = 9;
+constexpr int TESTID_11 = 11;
+
 class CommandStoreTest : public ::testing::Test
 {
 protected:
@@ -20,15 +24,90 @@ TEST_F(CommandStoreTest, StoreCommandTest)
     m_commandStore->Clear();
 
     // NOLINTNEXTLINE(cppcoreguidelines-avoid-magic-numbers)
-    command_store::Command cmd1(5, "Module1", "{CommandTextHERE}", "Parameter1", command_store::Status::IN_PROGRESS);
+    command_store::Command cmd1(
+        TESTID_5, "Module1", "{CommandTextHERE}", "Parameter1", "Result1", command_store::Status::IN_PROGRESS);
+    bool retVal = m_commandStore->StoreCommand(cmd1);
+
+    // NOLINTNEXTLINE(cppcoreguidelines-avoid-magic-numbers)
+    command_store::Command cmd2(
+        TESTID_9, "Module2", R"({"Some"="thing"})", "Parameter2", "Result2", command_store::Status::IN_PROGRESS);
+    retVal = m_commandStore->StoreCommand(cmd2);
+    ASSERT_EQ(retVal, true);
+
+    // NOLINTNEXTLINE(cppcoreguidelines-avoid-magic-numbers)
+    command_store::Command cmd3(
+        TESTID_5, "Module3", "{CommandTextHERE}", "Parameter3", "Result3", command_store::Status::IN_PROGRESS);
+    retVal = m_commandStore->StoreCommand(cmd3);
+    ASSERT_EQ(retVal, false);
+
+    ASSERT_EQ(m_commandStore->GetCount(), 2);
+}
+
+TEST_F(CommandStoreTest, UpdateCommandTest)
+{
+    m_commandStore->Clear();
+
+    // NOLINTNEXTLINE(cppcoreguidelines-avoid-magic-numbers)
+    command_store::Command cmd1(
+        TESTID_5, "Module1", "{CommandTextHERE}", "Parameter1", "Result1", command_store::Status::IN_PROGRESS);
     m_commandStore->StoreCommand(cmd1);
     // NOLINTNEXTLINE(cppcoreguidelines-avoid-magic-numbers)
-    command_store::Command cmd2(9, "Module2", R"({"Some"="thing"})", "Parameter2", command_store::Status::IN_PROGRESS);
+    command_store::Command cmd2(
+        TESTID_9, "Module2", R"({"Some"="thing"})", "Parameter2", "Result2", command_store::Status::IN_PROGRESS);
     m_commandStore->StoreCommand(cmd2);
-    // NOLINTNEXTLINE(cppcoreguidelines-avoid-magic-numbers)
-    command_store::Command cmd3(5, "Module3", "{CommandTextHERE}", "Parameter3", command_store::Status::IN_PROGRESS);
-    m_commandStore->StoreCommand(cmd3);
+
     ASSERT_EQ(m_commandStore->GetCount(), 2);
+
+    // NOLINTNEXTLINE(cppcoreguidelines-avoid-magic-numbers)
+    command_store::Command cmdUpdate(TESTID_9,
+                                     "Updated Module",
+                                     "Updated Command",
+                                     "Updated Parameter",
+                                     "Updated Result",
+                                     command_store::Status::SUCCESS);
+
+    bool retVal = m_commandStore->UpdateCommand(cmdUpdate);
+    ASSERT_EQ(retVal, true);
+
+    // NOLINTNEXTLINE(cppcoreguidelines-avoid-magic-numbers)
+    std::optional<command_store::Command> retValue = m_commandStore->GetCommand(9);
+    if (retValue.has_value())
+    {
+        const command_store::Command& cmd = retValue.value();
+        ASSERT_EQ(cmd.m_id, TESTID_9);
+        ASSERT_EQ(cmd.m_module, "Updated Module");
+        ASSERT_EQ(cmd.m_command, "Updated Command");
+        ASSERT_EQ(cmd.m_parameters, "Updated Parameter");
+        ASSERT_EQ(cmd.m_result, "Updated Result");
+        ASSERT_EQ(cmd.m_status, command_store::Status::SUCCESS);
+    }
+    else
+    {
+        FAIL();
+    }
+
+    // NOLINTNEXTLINE(cppcoreguidelines-avoid-magic-numbers)
+    command_store::Command cmdUpdate2(TESTID_9, "", "", "", "Newly Updated Result", command_store::Status::UNKNOWN);
+
+    retVal = m_commandStore->UpdateCommand(cmdUpdate2);
+    ASSERT_EQ(retVal, true);
+
+    // NOLINTNEXTLINE(cppcoreguidelines-avoid-magic-numbers)
+    retValue = m_commandStore->GetCommand(TESTID_9);
+    if (retValue.has_value())
+    {
+        const command_store::Command& cmd = retValue.value();
+        ASSERT_EQ(cmd.m_id, TESTID_9);
+        ASSERT_EQ(cmd.m_module, "Updated Module");
+        ASSERT_EQ(cmd.m_command, "Updated Command");
+        ASSERT_EQ(cmd.m_parameters, "Updated Parameter");
+        ASSERT_EQ(cmd.m_result, "Newly Updated Result");
+        ASSERT_EQ(cmd.m_status, command_store::Status::SUCCESS);
+    }
+    else
+    {
+        FAIL();
+    }
 }
 
 TEST_F(CommandStoreTest, DeleteCommandTest)
@@ -36,7 +115,7 @@ TEST_F(CommandStoreTest, DeleteCommandTest)
     int initialCount = m_commandStore->GetCount();
 
     // NOLINTNEXTLINE(cppcoreguidelines-avoid-magic-numbers)
-    m_commandStore->DeleteCommand(9);
+    m_commandStore->DeleteCommand(TESTID_9);
 
     ASSERT_EQ(m_commandStore->GetCount(), initialCount - 1);
 }
@@ -46,13 +125,16 @@ TEST_F(CommandStoreTest, GetCommandTest)
     m_commandStore->Clear();
 
     // NOLINTNEXTLINE(cppcoreguidelines-avoid-magic-numbers)
-    command_store::Command cmd1(5, "Module1", "{CommandTextHERE}", "Parameter1", command_store::Status::IN_PROGRESS);
+    command_store::Command cmd1(
+        TESTID_5, "Module1", "{CommandTextHERE}", "Parameter1", "Result1", command_store::Status::IN_PROGRESS);
     m_commandStore->StoreCommand(cmd1);
     // NOLINTNEXTLINE(cppcoreguidelines-avoid-magic-numbers)
-    command_store::Command cmd2(9, "Module2", "TestValue9", "Parameter2", command_store::Status::IN_PROGRESS);
+    command_store::Command cmd2(
+        TESTID_9, "Module2", "TestValue9", "Parameter2", "Result2", command_store::Status::IN_PROGRESS);
     m_commandStore->StoreCommand(cmd2);
     // NOLINTNEXTLINE(cppcoreguidelines-avoid-magic-numbers)
-    command_store::Command cmd3(11, "Module3", "{CommandTextHERE}", "Parameter3", command_store::Status::IN_PROGRESS);
+    command_store::Command cmd3(
+        TESTID_11, "Module3", "{CommandTextHERE}", "Parameter3", "Result3", command_store::Status::IN_PROGRESS);
     m_commandStore->StoreCommand(cmd3);
     ASSERT_EQ(m_commandStore->GetCount(), 3);
 
@@ -61,17 +143,17 @@ TEST_F(CommandStoreTest, GetCommandTest)
     if (retValue.has_value())
     {
         const command_store::Command& cmd = retValue.value();
-        ASSERT_EQ(cmd.m_id, 9);
+        ASSERT_EQ(cmd.m_id, TESTID_9);
         ASSERT_EQ(cmd.m_module, "Module2");
         ASSERT_EQ(cmd.m_command, "TestValue9");
     }
 
     // NOLINTNEXTLINE(cppcoreguidelines-avoid-magic-numbers)
-    retValue = m_commandStore->GetCommand(11);
+    retValue = m_commandStore->GetCommand(TESTID_11);
     if (retValue.has_value())
     {
         const command_store::Command& cmd = retValue.value();
-        ASSERT_EQ(cmd.m_id, 11);
+        ASSERT_EQ(cmd.m_id, TESTID_11);
         ASSERT_EQ(cmd.m_module, "Module3");
         ASSERT_EQ(cmd.m_command, "{CommandTextHERE}");
     }