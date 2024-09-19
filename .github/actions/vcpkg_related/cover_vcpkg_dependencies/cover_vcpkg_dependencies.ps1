1ec9f452a688ec6f7abfc6b1e551e495ae1f0ecd
Manuel <manuel.cano@wazuh.com>
2024-09-19 12:11:19 +0200

    feat: improved port-overlays by removing patches

--- .github/actions/vcpkg_related/cover_vcpkg_dependencies/cover_vcpkg_dependencies.ps1
+++
@@ -1,9 +0,0 @@
-.$(C:\vcpkg\vcpkg fetch nuget) `
-    sources add `
-    -Source "https://nuget.pkg.github.com/wazuh/index.json" `
-    -Name "GitHub" `
-    -UserName "wazuh" `
-    -Password "$Env:GITHUB_TOKEN"
-.$(C:\vcpkg\vcpkg fetch nuget) `
-    setapikey "$Env:GITHUB_TOKEN" `
-    -Source "https://nuget.pkg.github.com/wazuh/index.json"
--- src/ports-overlay/procps/portfile.cmake
+++ src/ports-overlay/procps/portfile.cmake
@@ -4,14 +4,15 @@ vcpkg_from_github(
   REF fadce3f1b25b55d5f22be0d26db1ae2a53018255
   SHA512 c4efb9583be0e2272316b57ab9e574698d78a0bb6b2a84d4e5857e1fde9418e6fb1354892a1fba799a456e03eeb4b995487673d88d2d0b5b19087c6a812cd500
   HEAD_REF v3.3.0
-  PATCHES
-    set_procps_cmake_project.patch
 )
 
-vcpkg_cmake_configure(
+vcpkg_configure_make(
   SOURCE_PATH "${SOURCE_PATH}"
 )
 
-vcpkg_cmake_install()
+vcpkg_install_make()
 
 vcpkg_copy_pdbs()
+
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
+set(VCPKG_POLICY_SKIP_COPYRIGHT_CHECK enabled)
