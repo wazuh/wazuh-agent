0a86e7626de09964c45715061ee7191638a60748
Ariel Martin <ariel.martin@wazuh.com>
2024-10-04 12:11:28 -0300

    feat: use of new classes and functions for managing agent

--- src/agent/src/main.cpp
+++ src/agent/src/main.cpp
@@ -1,70 +1,49 @@
-#include <agent.hpp>
-#include <agent_info.hpp>
-#include <agent_registration.hpp>
+#include "process_options.hpp"
 #include <cmd_ln_parser.hpp>
-#include <http_client.hpp>
 #include <logger.hpp>
-
-#include <optional>
 
 int main(int argc, char* argv[])
 {
     Logger logger;
-    LogInfo("Starting Wazuh Agent.");
+    CommandlineParser cmdParser(argc, argv);
+
+    LogInfo("Starting wazuh-agent service.");
 
     try
     {
-        CommandlineParser cmdParser(argc, argv);
-
-        if (cmdParser.OptionExists("--register"))
+        if (cmdParser.OptionExists("register"))
         {
-            LogInfo("Starting registration process");
-
-            if (cmdParser.OptionExists("--user") && cmdParser.OptionExists("--password") &&
-                cmdParser.OptionExists("--key"))
-            {
-                const auto user = cmdParser.GetOptionValue("--user");
-                const auto password = cmdParser.GetOptionValue("--password");
-
-                AgentInfo agentInfo;
-                agentInfo.SetKey(cmdParser.GetOptionValue("--key"));
-
-                if (cmdParser.OptionExists("--name"))
-                {
-                    agentInfo.SetName(cmdParser.GetOptionValue("--name"));
-                }
-                else
-                {
-                    agentInfo.SetName(boost::asio::ip::host_name());
-                }
-
-                http_client::HttpClient httpClient;
-
-                agent_registration::AgentRegistration reg(user, password, agentInfo.GetKey(), agentInfo.GetName());
-                if (reg.Register(httpClient))
-                {
-                    LogInfo("Agent registered.");
-                }
-                else
-                {
-                    LogError("Registration fail.");
-                }
-            }
-            else
-            {
-                LogError("--user, --password and --key args are mandatory");
-            }
-
-            LogInfo("Exiting ...");
-            return 0;
+            RegisterAgent(cmdParser.GetOptionValue("--user"),
+                          cmdParser.GetOptionValue("--password"),
+                          cmdParser.GetOptionValue("--key"),
+                          cmdParser.GetOptionValue("--name"));
         }
+        else if (cmdParser.OptionExists("start"))
+        {
+            StartAgent();
+        }
+        else if (cmdParser.OptionExists("restart"))
+        {
+            RestartAgent();
+        }
+        else if (cmdParser.OptionExists("status"))
+        {
+            StatusAgent();
+        }
+        else if (cmdParser.OptionExists("stop"))
+        {
+            StopAgent();
+        }
+        else
+        {
+            PrintHelp();
+        }
+
+        return 0;
     }
     catch (const std::exception& e)
     {
-        LogError("An error occurred: {}.", e.what());
+        LogCritical("An error occurred: {}.", e.what());
         return 1;
     }
-
-    Agent agent;
-    agent.Run();
 }
