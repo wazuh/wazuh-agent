c28f3028f3f2cb88085fe166a6593ee79a60fbdf
jr0me <joaquin.romera@wazuh.com>
2024-10-08 17:51:58 -0300

    feat: add module wrapper interface methods to CentralizedConfiguration
    At the moment just Start, Stop and Name.
    
    This class will not be performing any specific actions on
    start and stop, at least for this PR.

--- src/modules/centralized_configuration/include/centralized_configuration.hpp
+++ src/modules/centralized_configuration/include/centralized_configuration.hpp
@@ -1,8 +1,14 @@
 #pragma once
 
+#include <string>
+
 namespace centralized_configuration
 {
     class CentralizedConfiguration
     {
+    public:
+        void Start() const;
+        void Stop() const;
+        std::string Name() const;
     };
 } // namespace centralized_configuration
--- src/modules/centralized_configuration/src/centralized_configuration.cpp
+++ src/modules/centralized_configuration/src/centralized_configuration.cpp
@@ -1,2 +1,17 @@
 #include <centralized_configuration.hpp>
 
+namespace centralized_configuration
+{
+    void CentralizedConfiguration::Start() const
+    {
+    }
+
+    void CentralizedConfiguration::Stop() const
+    {
+    }
+
+    std::string CentralizedConfiguration::Name() const
+    {
+        return "CentralizedConfiguration";
+    }
+} // namespace centralized_configuration
