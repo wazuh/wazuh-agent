8c3687752b76256d4b91277dbfbd4c1c5e506f17
jr0me <joaquin.romera@wazuh.com>
2024-10-09 09:24:57 -0300

    feat: implement CentralizedConfiguration::Setup
    It's not necessary to set any configuration at the moment

--- src/modules/centralized_configuration/tests/centralized_configuration_tests.cpp
+++ src/modules/centralized_configuration/tests/centralized_configuration_tests.cpp
@@ -1,6 +1,7 @@
 #include <gtest/gtest.h>
 
 #include <centralized_configuration.hpp>
+#include <configuration_parser.hpp>
 
 using centralized_configuration::CentralizedConfiguration;
 
@@ -17,6 +18,10 @@ TEST(CentralizedConfiguration, ImplementsModuleWrapperInterface)
     EXPECT_NO_THROW(centralizedConfiguration.Start());
     EXPECT_NO_THROW(centralizedConfiguration.Stop());
     EXPECT_NO_THROW(centralizedConfiguration.Name());
+
+    const std::string emptyConfig;
+    configuration::ConfigurationParser configurationParser(emptyConfig);
+    EXPECT_NO_THROW(centralizedConfiguration.Setup(configurationParser));
 }
 
 int main(int argc, char** argv)
