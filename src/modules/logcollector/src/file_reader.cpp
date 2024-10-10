5bd06b21dd84b2cacd572bda93674430848c2bb3
Vikman Fernandez-Castro <vmfdez90@gmail.com>
2024-10-10 18:26:36 +0200

    feat: logcollector: add UNIX file reader

--- src/modules/logcollector/src/file_reader.cpp
+++ src/modules/logcollector/src/file_reader.cpp
@@ -1,11 +1,24 @@
 #include "file_reader.hpp"
 #include <logcollector.hpp>
+#include <logger.hpp>
 
+#include <string>
 #include <boost/asio/co_spawn.hpp>
 #include <boost/asio/detached.hpp>
+#include <boost/asio/posix/stream_descriptor.hpp>
+
+// POSIX only
+#include <fcntl.h>
+#include <cerrno>
+#include <cstring>
 
 using namespace logcollector;
 using namespace std;
+using boost::asio::posix::stream_descriptor;
+using boost::asio::mutable_buffer;
+using boost::asio::use_awaitable;
+
+constexpr auto BUFFER_SIZE = 4096;
 
 Awaitable FileReader::run() {
     auto & logcollector = Logcollector::Instance();
@@ -14,13 +27,51 @@ Awaitable FileReader::run() {
     auto lf = Localfile(path);
     logcollector.EnqueueTask(lf.run());
 
+    // TODO: Resolve wildcards
+    // TODO: Check for file rotation
+
     co_return;
 }
 
-Localfile::Localfile(string filePath) : m_filePath(std::move(filePath)) { }
+Localfile::Localfile(string filePath) :
+    m_filePath(std::move(filePath)),
+    m_offset(0),
+    m_inode(0) { }
 
 Awaitable Localfile::run() {
+
+    // TODO: Save/load file state
+
     auto & logcollector = Logcollector::Instance();
     logcollector.SendMessage("test", "Hello World (from Localfile)");
+
+    auto fd = open(m_filePath.c_str(), O_RDONLY | O_CLOEXEC, 0);
+
+    if (fd == -1) {
+        LogWarn("Cannot open file '{}': {}", m_filePath, strerror(errno));
+        co_return;
+    }
+
+    auto file = stream_descriptor(logcollector.GetExecutor(), fd);
+    string data;
+
+    while (true) {
+        char buffer[BUFFER_SIZE];
+        std::size_t n = co_await file.async_read_some(boost::asio::buffer(buffer), use_awaitable);
+
+        if (n == 0) {
+            continue;
+        }
+
+        data.append(buffer, n);
+        std::size_t newline_pos = 0;
+
+        while ((newline_pos = data.find('\n')) != std::string::npos) {
+            auto line = data.substr(0, newline_pos);
+            logcollector.SendMessage(m_filePath, line);
+            data.erase(0, newline_pos + 1);
+        }
+    }
+
     co_return;
 }
--- src/modules/logcollector/src/file_reader.hpp
+++ src/modules/logcollector/src/file_reader.hpp
@@ -13,11 +13,20 @@ public:
 
 private:
     std::string m_filePath;
+    unsigned long m_offset;
+    unsigned long m_inode;
 };
 
 class FileReader : public IReader {
 public:
     Awaitable run();
+
+private:
+    std::string m_fileGlob;
+    int m_ageThreshold;
+    std::string delimRegex;
+    bool m_useBookmark;
+    std::vector<Localfile> m_localfiles;
 };
 
 }
