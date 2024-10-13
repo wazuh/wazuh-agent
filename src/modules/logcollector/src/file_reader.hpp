495a8da7a570635ffab3ee38e91bcd7f16e650ef
Vikman Fernandez-Castro <vmfdez90@gmail.com>
2024-10-13 13:39:38 +0200

    refactor(logcollector): split FileReader logic into smaller methods

--- src/modules/logcollector/src/file_reader.hpp
+++ src/modules/logcollector/src/file_reader.hpp
@@ -1,20 +1,25 @@
 #pragma once
 
 #include <vector>
+#include <boost/asio/posix/stream_descriptor.hpp>
 
 #include "reader.hpp"
 
 namespace logcollector {
 
+
 class Localfile {
 public:
-    Localfile(std::string filePath);
+    Localfile(std::string filePath, boost::asio::any_io_executor executor);
     Awaitable run();
 
 private:
+    Awaitable Follow();
+    void SplitData(std::string & accumulator);
     std::string m_filePath;
     unsigned long m_offset;
     unsigned long m_inode;
+    boost::asio::posix::stream_descriptor m_file;
 };
 
 class FileReader : public IReader {
