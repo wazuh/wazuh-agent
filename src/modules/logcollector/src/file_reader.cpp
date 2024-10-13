6484d51a851df56a7b404ff810b27f108f03a14a
Vikman Fernandez-Castro <vmfdez90@gmail.com>
2024-10-13 13:39:38 +0200

    refactor(logcollector): split FileReader logic into smaller methods

--- src/modules/logcollector/src/file_reader.cpp
+++ src/modules/logcollector/src/file_reader.cpp
@@ -6,7 +6,6 @@
 #include <vector>
 #include <boost/asio/co_spawn.hpp>
 #include <boost/asio/detached.hpp>
-#include <boost/asio/posix/stream_descriptor.hpp>
 
 // POSIX only
 #include <fcntl.h>
@@ -22,9 +21,10 @@ constexpr auto FILE_WAIT = std::chrono::milliseconds(500);
 
 Awaitable FileReader::run() {
     auto & logcollector = Logcollector::Instance();
+    auto executor = co_await this_coro::executor;
 
-    m_localfiles.emplace_back("/var/log/syslog");
-    m_localfiles.emplace_back("/root/test/test.log");
+    m_localfiles.emplace_back("/var/log/syslog", executor);
+    m_localfiles.emplace_back("/root/test/test.log", executor);
 
     for (auto & lf : m_localfiles) {
         logcollector.EnqueueTask(lf.run());
@@ -32,21 +32,17 @@ Awaitable FileReader::run() {
 
     // TODO: Resolve wildcards
     // TODO: Check for file rotation
-
-    co_return;
 }
 
-Localfile::Localfile(string filePath) :
+Localfile::Localfile(std::string filePath, boost::asio::any_io_executor executor) :
     m_filePath(std::move(filePath)),
     m_offset(0),
-    m_inode(0) { }
+    m_inode(0),
+    m_file(executor) { }
 
 Awaitable Localfile::run() {
-
     // TODO: Save/load file state
 
-    auto & logcollector = Logcollector::Instance();
-    auto executor = co_await this_coro::executor;
     auto fd = open(m_filePath.c_str(), O_RDONLY | O_CLOEXEC, 0);
 
     if (fd == -1) {
@@ -55,15 +51,21 @@ Awaitable Localfile::run() {
     }
 
     lseek(fd, 0, SEEK_END);
-    auto file = posix::stream_descriptor(executor, fd);
+    m_file.assign(fd);
+
+    co_await Follow();
+}
+
+Awaitable Localfile::Follow() {
+    auto executor = co_await this_coro::executor;
     auto chunk = vector<char>(BUFFER_SIZE);
-    string data;
+    string accumulator;
 
     while (true) {
         size_t n = 0;
 
         try {
-            n = co_await file.async_read_some(buffer(chunk), use_awaitable);
+            n = co_await m_file.async_read_some(buffer(chunk), use_awaitable);
         } catch (boost::system::system_error &) {
             n = 0;
         }
@@ -73,15 +75,18 @@ Awaitable Localfile::run() {
             continue;
         }
 
-        data.append(chunk.data(), n);
-        std::size_t newline_pos = 0;
-
-        while ((newline_pos = data.find('\n')) != string::npos) {
-            auto line = data.substr(0, newline_pos);
-            logcollector.SendMessage(m_filePath, line);
-            data.erase(0, newline_pos + 1);
-        }
+        accumulator.append(chunk.data(), n);
+        SplitData(accumulator);
     }
+}
 
-    co_return;
+void Localfile::SplitData(string & accumulator) {
+    auto & logcollector = Logcollector::Instance();
+    std::size_t newline_pos = 0;
+
+    while ((newline_pos = accumulator.find('\n')) != string::npos) {
+        auto line = accumulator.substr(0, newline_pos);
+        logcollector.SendMessage(m_filePath, line);
+        accumulator.erase(0, newline_pos + 1);
+    }
 }
