c7c0e90fd58b1f2091906ab78b6d5a1d5e2c1898
Nicolas Gomez Palacios <nicogp2@gmail.com>
2024-10-18 18:45:38 -0300

    feat: added --config-path option in PrinHelp function

--- src/agent/src/process_options_unix.cpp
+++ src/agent/src/process_options_unix.cpp
@@ -58,18 +58,20 @@ void StopAgent()
 
 void PrintHelp()
 {
-    LogInfo("Wazuh Agent help.");
-
     // TO DO
+    std::cout << "Wazuh Agent help.\n ";
     std::cout << "Usage: wazuh-agent [options]\n";
     std::cout << "\n";
     std::cout << "Options:\n";
     std::cout << "     --run                Start wazuh-agent\n";
+    std::cout << "       [--config-path <path>]   Specify configuration file path (optional)\n";
     std::cout << "     --start              Start wazuh-agent daemon\n";
     std::cout << "     --status             Get wazuh-agent daemon status\n";
     std::cout << "     --stop               Stop wazuh-agent daemon\n";
     std::cout << "     --restart            Restart wazuh-agent daemon\n";
+    std::cout << "       [--config-path <path>]   Specify configuration file path (optional)\n";
     std::cout << "     --register-agent     Register wazuh-agent\n";
+    std::cout << "       [--config-path <path>]   Specify configuration file path (optional)\n";
     std::cout << "     --help               This help message\n";
 }
 
