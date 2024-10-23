93afbd8241bf25ccf4abc9777278e20c047efe52
Santi <sdvendramini@gmail.com>
2024-10-23 15:28:24 +0200

    test: Tests added for RemoveService function

---
+++ src/agent/tests/windows_service_test.cpp
@@ -0,0 +1,78 @@
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+#include <windows_service.cpp>
+
+#include <iwindows_api_facade.hpp>
+
+#define WIN32_LEAN_AND_MEAN
+#include <windows.h>
+
+class MockWindowsApiFacade : public windows_api_facade::IWindowsApiFacade
+{
+public:
+    MOCK_METHOD(void*, OpenSCM, (unsigned int desiredAccess), (override));
+    MOCK_METHOD(void*, OpenSvc, (void* sHandle, std::string sName, unsigned int desiredAccess), (override));
+    MOCK_METHOD(bool, DeleteSvc, (void* sHandle), (override));
+};
+
+class WindowsServiceTest : public ::testing::Test
+{
+protected:
+    MockWindowsApiFacade mockWindowsApiFacade;
+
+    void SetUp() override {}
+
+    void TearDown() override {}
+};
+
+TEST_F(WindowsServiceTest, RemoveService_FailOpenSCM)
+{
+    EXPECT_CALL(mockWindowsApiFacade, OpenSCM(SC_MANAGER_CREATE_SERVICE)).WillOnce(testing::Return(nullptr));
+
+    bool result = WindowsService::RemoveService(mockWindowsApiFacade);
+
+    EXPECT_FALSE(result);
+}
+
+TEST_F(WindowsServiceTest, RemoveService_FailOpenSvc)
+{
+    EXPECT_CALL(mockWindowsApiFacade, OpenSCM(SC_MANAGER_CREATE_SERVICE))
+        .WillOnce(testing::Return(reinterpret_cast<SC_HANDLE>(1)));
+    EXPECT_CALL(mockWindowsApiFacade, OpenSvc(testing::_, testing::_, DELETE)).WillOnce(testing::Return(nullptr));
+
+    bool result = WindowsService::RemoveService(mockWindowsApiFacade);
+
+    EXPECT_FALSE(result);
+}
+
+TEST_F(WindowsServiceTest, RemoveService_FailDeleteSvc)
+{
+    EXPECT_CALL(mockWindowsApiFacade, OpenSCM(SC_MANAGER_CREATE_SERVICE))
+        .WillOnce(testing::Return(reinterpret_cast<SC_HANDLE>(1)));
+    EXPECT_CALL(mockWindowsApiFacade, OpenSvc(testing::_, testing::_, DELETE))
+        .WillOnce(testing::Return(reinterpret_cast<SC_HANDLE>(1)));
+    EXPECT_CALL(mockWindowsApiFacade, DeleteSvc(reinterpret_cast<SC_HANDLE>(1))).WillOnce(testing::Return(false));
+
+    bool result = WindowsService::RemoveService(mockWindowsApiFacade);
+
+    EXPECT_FALSE(result);
+}
+
+TEST_F(WindowsServiceTest, RemoveService_Success)
+{
+    EXPECT_CALL(mockWindowsApiFacade, OpenSCM(SC_MANAGER_CREATE_SERVICE))
+        .WillOnce(testing::Return(reinterpret_cast<SC_HANDLE>(1)));
+    EXPECT_CALL(mockWindowsApiFacade, OpenSvc(testing::_, testing::_, DELETE))
+        .WillOnce(testing::Return(reinterpret_cast<SC_HANDLE>(1)));
+    EXPECT_CALL(mockWindowsApiFacade, DeleteSvc(reinterpret_cast<SC_HANDLE>(1))).WillOnce(testing::Return(true));
+
+    bool result = WindowsService::RemoveService(mockWindowsApiFacade);
+
+    EXPECT_TRUE(result);
+}
+
+int main(int argc, char** argv)
+{
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
