9060348ed7da07dcfa1e6a7ac810d342fb551b04
jr0me <joaquin.romera@wazuh.com>
2024-10-24 18:57:36 -0300

    feat: add DispatchCommand overload, use it for CentralizedConfiguration

--- src/agent/src/command_handler_utils.hpp
+++ src/agent/src/command_handler_utils.hpp
@@ -7,8 +7,14 @@
 
 #include <boost/asio/awaitable.hpp>
 
+#include <functional>
 #include <memory>
 #include <tuple>
+
+boost::asio::awaitable<module_command::CommandExecutionResult> DispatchCommand(
+    module_command::CommandEntry commandEntry,
+    std::function<boost::asio::awaitable<module_command::CommandExecutionResult>(std::string command)> executeFunction,
+    std::shared_ptr<IMultiTypeQueue> messageQueue);
 
 boost::asio::awaitable<module_command::CommandExecutionResult>
 DispatchCommand(module_command::CommandEntry commandEntry,
