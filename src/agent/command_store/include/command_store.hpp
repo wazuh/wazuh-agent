ed4736a57185a7b0706ddb68c460bf1aefeebb21
Tomas Turina <tomas.turina@wazuh.com>
2024-11-01 19:32:08 +0000

    feat(agent): document command hpp files

--- src/agent/command_store/include/command_store.hpp
+++ src/agent/command_store/include/command_store.hpp
@@ -12,22 +12,56 @@ namespace command_store
     const std::string COMMANDSTORE_DEFAULT_DB_PATH = "command_store.db";
     const std::string COMMANDSTORE_TABLE_NAME = "COMMAND";
 
+    /// @brief CommandStore class
+    ///
+    /// This class provides methods for storing, retrieving, and deleting commands
+    /// in the command store database. It uses a SQLite database to store the
+    /// commands.
     class CommandStore
     {
     private:
+        /// @brief The SQLite database object
         std::unique_ptr<sqlite_manager::SQLiteManager> m_dataBase;
 
+        /// @brief Gets the current timestamp in seconds
+        /// @return The current timestamp in seconds
         double GetCurrentTimestampAsReal();
+
+        /// @brief Converts an integer to a module_command::Status value
+        /// @param i The integer to convert
+        /// @return The module_command::Status value corresponding
         module_command::Status StatusFromInt(const int i);
 
     public:
+        /// @brief CommandStore constructor
         CommandStore();
 
+        /// @brief Clears all commands from the database
+        /// @return True if successful, false otherwise
         bool Clear();
+
+        /// @brief Gets the count of commands in the database
+        /// @return The number of commands in the database
         int GetCount();
+
+        /// @brief Stores a command in the database
+        /// @param cmd The command to store
+        /// @return True if successful, false otherwise
         bool StoreCommand(const module_command::CommandEntry& cmd);
+
+        /// @brief Deletes a command from the database by ID
+        /// @param id The ID of the command to delete
+        /// @return True if successful, false otherwise
         bool DeleteCommand(const std::string& id);
+
+        /// @brief Retrieves a command from the database by ID
+        /// @param id The ID of the command to retrieve
+        /// @return An optional containing the command if found, or nullopt if not
         std::optional<module_command::CommandEntry> GetCommand(const std::string& id);
+
+        /// @brief Updates an existing command in the database
+        /// @param cmd The updated command data
+        /// @return True if successful, false otherwise
         bool UpdateCommand(const module_command::CommandEntry& cmd);
     };
 } // namespace command_store
