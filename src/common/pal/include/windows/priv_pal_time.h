e4e5b3f9965b6d9c2d51a9f2cf74b29e5c2de53d
ncvicchi <norberto.vicchi@wazuh.com>
2024-11-19 15:44:57 -0300

    feat: localtime_r implementation

--- src/common/pal/include/windows/priv_pal_time.h
+++ src/common/pal/include/windows/priv_pal_time.h
@@ -1,5 +1,9 @@
 #pragma once
 
+#include <time.h>
+
 #if !defined(PRIV_PAL_H_REQUIRED) || !defined(PAL_H_REQUIRED)
 #error "Do not include this file. Use pal.h instead"
 #endif
+
+struct tm *localtime_r(const time_t *timer, struct tm *result);
