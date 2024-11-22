wazuh/wazuh

fix: added a CloseHandle function in realtime code, warn switching to schedule

src/syscheckd/src/run_realtime.c
@@ -689,7 +689,15 @@ int realtime_adddir(const char *dir, directory_t *configuration) {
 
     /* Add directory to be monitored */
     if(realtime_win32read(rtlocald) == 0) {
-        mdebug1(FIM_REALTIME_DIRECTORYCHANGES, rtlocald->dir);
+        DWORD last_error = GetLastError();
+        mdebug1(FIM_REALTIME_DIRECTORYCHANGES, rtlocald->dir, last_error, win_strerror(last_error));
+        CloseHandle(rtlocald->h);
+        rtlocald->watch_status = FIM_RT_HANDLE_CLOSED;
+        if (!w_directory_exists(rtlocald->dir)) {
+            mwarn(FIM_REALTIME_FILE_NOT_SUPPORTED, rtlocald->dir);
+            configuration->options &= ~REALTIME_ACTIVE;
+            configuration->options |= SCHEDULED_ACTIVE;
+        }
         free_win32rtfim_data(rtlocald);
 
         w_mutex_unlock(&syscheck.fim_realtime_mutex);

wazuh/wazuh-agent

src/modules/fim/src/run_realtime.c
@@ -688,7 +688,15 @@ int realtime_adddir(const char *dir, directory_t *configuration) {
 
     /* Add directory to be monitored */
     if(realtime_win32read(rtlocald) == 0) {
-        LogDebug(FIM_REALTIME_DIRECTORYCHANGES, rtlocald->dir);
+        DWORD last_error = GetLastError();
+        LogDebug(FIM_REALTIME_DIRECTORYCHANGES, rtlocald->dir, last_error, win_strerror(last_error));
+        CloseHandle(rtlocald->h);
+        rtlocald->watch_status = FIM_RT_HANDLE_CLOSED;
+        if (!w_directory_exists(rtlocald->dir)) {
+            LogWarn(FIM_REALTIME_FILE_NOT_SUPPORTED, rtlocald->dir);
+            configuration->options &= ~REALTIME_ACTIVE;
+            configuration->options |= SCHEDULED_ACTIVE;
+        }
         free_win32rtfim_data(rtlocald);
 
         w_mutex_unlock(&syscheck.fim_realtime_mutex);