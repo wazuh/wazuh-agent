34cf92b9226c21cb9489a118a380318526857a2b
Vikman Fernandez-Castro <vmfdez90@gmail.com>
2024-12-11 17:54:05 +0100

    test(inventory): check delta message on delete operation

--- src/modules/inventory/tests/inventory/inventory_test.cpp
+++ src/modules/inventory/tests/inventory/inventory_test.cpp
@@ -44,6 +44,32 @@ TEST_F(InventoryTest, SendUpdateEvent) {
     inventory.SendDeltaEvent(inputData);
 }
 
+TEST_F(InventoryTest, SendDeleteEvent) {
+    ::testing::MockFunction<int(const Message&)> mockPushMessage;
+
+    inventory.SetPushMessageFunction(mockPushMessage.AsStdFunction());
+
+    EXPECT_CALL(mockPushMessage, Call(::testing::_))
+        .WillOnce([](const Message& msg)
+        {
+            auto expectedData = R"({})";
+            auto expectedMetadata = R"({"id":"123","module":"inventory","operation":"delete","type":"hardware"})";
+
+            EXPECT_EQ(msg.data.dump(), expectedData);
+            EXPECT_EQ(msg.metaData, expectedMetadata);
+            return 1;
+        });
+
+    auto inputData = R"({
+        "type": "hardware",
+        "operation": "delete",
+        "id": "123",
+        "data": {"key": "value"}
+    })";
+
+    inventory.SendDeltaEvent(inputData);
+}
+
 int main(int argc, char** argv)
 {
     ::testing::InitGoogleTest(&argc, argv);
