ac5092338d48ddbc22699ebf56a03c9da69aa3f4
Nicolas Gomez Palacios <nicogp2@gmail.com>
2024-12-12 14:47:25 -0300

    feat: gets the validation_mode parameter from the configuration

--- src/agent/src/process_options.cpp
+++ src/agent/src/process_options.cpp
@@ -21,11 +21,24 @@ void RegisterAgent(const std::string& url,
     auto dbFolderPath =
         configurationParser.GetConfig<std::string>("agent", "path.data").value_or(config::DEFAULT_DATA_PATH);
 
+    auto verificationMode = configurationParser.GetConfig<std::string>("agent", "verification_mode")
+                                .value_or(config::agent::DEFAULT_VERIFICATION_MODE);
+
     if (!url.empty() && !user.empty() && !password.empty())
     {
         try
         {
             std::cout << "Starting wazuh-agent registration\n";
+
+            if (std::find(std::begin(config::agent::VALID_VERIFICATION_MODES),
+                          std::end(config::agent::VALID_VERIFICATION_MODES),
+                          verificationMode) == std::end(config::agent::VALID_VERIFICATION_MODES))
+            {
+                LogWarn("Incorrect value for 'verification_mode', in case of HTTPS connections the default value '{}' "
+                        "is used.",
+                        config::agent::DEFAULT_VERIFICATION_MODE);
+                verificationMode = config::agent::DEFAULT_VERIFICATION_MODE;
+            }
 
             agent_registration::AgentRegistration reg(url, user, password, key, name, dbFolderPath);
 
