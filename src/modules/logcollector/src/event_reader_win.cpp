6dbeaf545eb6cf5cf8a420501e3a32d2dff47857
LucioDonda <donda.lucio@gmail.com>
2025-01-09 19:32:53 +0000

    feat: removing bookmark feature

--- src/modules/logcollector/src/event_reader_win.cpp
+++ src/modules/logcollector/src/event_reader_win.cpp
@@ -13,8 +13,6 @@ void AddPlatformSpecificReader(std::shared_ptr<const configuration::Configuratio
 {
     const auto refreshInterval = configurationParser->GetConfig<time_t>("logcollector", "channel_refresh").value_or(config::logcollector::CHANNEL_REFRESH_INTERVAL);
 
-    const auto bookmarkEnabled = configurationParser->GetConfig<bool>("logcollector", "use_bookmark").value_or(config::logcollector::DEFAULT_USE_BOOKMARK);
-
     const auto windowsConfig = configurationParser->GetConfig<std::vector<std::map<std::string, std::string>>>("logcollector", "windows").value_or(
         std::vector<std::map<std::string, std::string>> {});
 
@@ -29,23 +27,11 @@ void AddPlatformSpecificReader(std::shared_ptr<const configuration::Configuratio
 WindowsEventTracerReader::WindowsEventTracerReader(Logcollector &logcollector,
                            const std::string channel,
                            const std::string query,
-                           const std::time_t channelRefreshInterval,
-                           bool bookmarkEnabled) :
+                           const std::time_t channelRefreshInterval) :
     IReader(logcollector),
     m_channel(channel),
     m_query(query),
-    m_ChannelsRefreshInterval(channelRefreshInterval),
-    m_bookmarkEnabled(bookmarkEnabled)
-    {
-        if(m_bookmarkEnabled)
-        {
-            // Creates a single file per instance
-            std::string filePath = config::DEFAULT_DATA_PATH;
-            std::string sanitizedChannel = channel;
-            std::replace(sanitizedChannel.begin(), sanitizedChannel.end(), '\\', '_');
-            m_bookmarkFile = filePath + "\\" + sanitizedChannel + Base64Encode(query) + ".bmk";;
-        }
-    }
+    m_ChannelsRefreshInterval(channelRefreshInterval) { }
 
 Awaitable WindowsEventTracerReader::Run()
 {
@@ -60,8 +46,6 @@ void WindowsEventTracerReader::Stop()
 
 Awaitable WindowsEventTracerReader::QueryEvents(const std::string channel, const std::string query)
 {
-    EVT_HANDLE bookmarkHandle = LoadBookmark();
-
     std::wstring wideStringChannel = std::wstring(channel.begin(), channel.end());
     std::wstring wideStringQuery = std::wstring(query.begin(), query.end());
 
@@ -97,19 +81,15 @@ Awaitable WindowsEventTracerReader::QueryEvents(const std::string channel, const
         nullptr,
         nullptr,
         wideStringChannel.c_str(),
-        wideStringQuery.empty() ? nullptr : wideStringQuery.c_str(),
-        bookmarkHandle,
+        wideStringQuery.c_str(),
+        NULL,
         this,
         subscriptionCallback,
-        m_bookmarkEnabled && bookmarkHandle? EvtSubscribeStartAfterBookmark : EvtSubscribeStartAtOldestRecord);
+        EvtSubscribeToFutureEvents); // EvtSubscribeStartAtOldestRecord
 
     if (!subscriptionHandle)
     {
         LogError("Failed to subscribe to event log: {}", std::to_string(GetLastError()));
-        if (bookmarkHandle)
-        {
-            EvtClose(bookmarkHandle);
-        }
         co_return;
     }
 
@@ -121,10 +101,6 @@ Awaitable WindowsEventTracerReader::QueryEvents(const std::string channel, const
     }
 
     LogInfo("Unsubscribing to channel '{}'.", channel);
-    if (bookmarkHandle)
-    {
-        EvtClose(bookmarkHandle);
-    }
     if (subscriptionHandle)
     {
         EvtClose(subscriptionHandle);
@@ -158,63 +134,6 @@ void WindowsEventTracerReader::ProcessEvent(EVT_HANDLE event, const std::string 
     }
 }
 
-EVT_HANDLE WindowsEventTracerReader::CreateBookmark(EVT_HANDLE event, EVT_HANDLE existingBookmark)
-{
-    EVT_HANDLE bookmark = existingBookmark ? existingBookmark : EvtCreateBookmark(NULL);
-    if (!EvtUpdateBookmark(bookmark, event))
-    {
-        LogError("Failed to update bookmark: {}", std::to_string(GetLastError()));
-    }
-    return bookmark;
-}
-
-void WindowsEventTracerReader::SaveBookmark(EVT_HANDLE bookmarkHandle)
-{
-    DWORD bufferUsed = 0;
-
-    EvtRender(NULL, bookmarkHandle, EvtRenderBookmark, 0, NULL, &bufferUsed, NULL);
-    std::vector<wchar_t> buffer(bufferUsed);
-    if (EvtRender(NULL, bookmarkHandle, EvtRenderBookmark, bufferUsed, buffer.data(), &bufferUsed, NULL))
-    {
-        std::wofstream file(m_bookmarkFile);
-        if (file.is_open())
-        {
-            file.write(buffer.data(), bufferUsed / sizeof(wchar_t));
-            file.close();
-        }
-    }
-}
-
-EVT_HANDLE WindowsEventTracerReader::LoadBookmark()
-{
-    if(m_bookmarkEnabled)
-    {
-        std::wifstream file(m_bookmarkFile);
-        if (file.is_open())
-        {
-            std::wstringstream buffer;
-            buffer << file.rdbuf();
-            std::wstring bookmarkXML = buffer.str();
-            file.close();
-            if(bookmarkXML.empty())
-            {
-                LogTrace("Empty bookmark file");
-            }
-            else
-            {
-                LogInfo("Creating bookark");
-                return EvtCreateBookmark(bookmarkXML.c_str());
-            }
-        }
-        else
-        {
-            LogWarn("Couldn't open bookmark file '{}'.",m_bookmarkFile);
-        }
-    }
-
-    return NULL;
-}
-
 std::string WindowsEventTracerReader::WcharVecToString(std::vector<wchar_t>& buffer)
 {
     buffer.erase(std::remove(buffer.begin(), buffer.end(), L'\0'), buffer.end());
@@ -225,36 +144,6 @@ std::string WindowsEventTracerReader::WcharVecToString(std::vector<wchar_t>& buf
                 [](wchar_t wc) -> char {
                     return static_cast<char>(wc);
                 });
-    return result;
-}
-
-std::string WindowsEventTracerReader::Base64Encode(const std::string& input)
-{
-    static const char* chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
-    std::string result;
-    unsigned int val = 0;
-    int valb = -6;
-    for (unsigned char c : input)
-    {
-        val = (val << 8) + c;
-        valb += 8;
-        while (valb >= 0)
-        {
-            result.push_back(chars[(val >> valb) & 0x3F]);
-            valb -= 6;
-        }
-    }
-
-    if (valb > -6)
-    {
-        result.push_back(chars[((val << 8) >> (valb + 8)) & 0x3F]);
-    }
-
-    while (result.size() % 4)
-    {
-        result.push_back('=');
-    }
-
     return result;
 }
 
