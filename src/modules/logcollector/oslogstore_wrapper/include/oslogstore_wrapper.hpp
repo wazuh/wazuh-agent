189739c7514f793024894348aa82439598b3267d
jr0me <joaquin.romera@wazuh.com>
2025-01-14 23:33:34 -0300

    feat: add OSLogStoreWrapper for accessing and enumerating OS logs on Apple platforms

---
+++ src/modules/logcollector/oslogstore_wrapper/include/oslogstore_wrapper.hpp
@@ -0,0 +1,54 @@
+#pragma once
+
+#include <memory>
+#include <string>
+#include <vector>
+
+/// @class OSLogStoreWrapper
+/// @brief A C++ wrapper for accessing and enumerating OS logs using OSLogStore APIs.
+///
+/// This class provides an interface to retrieve and manage system log entries based on specified criteria such as
+/// time range, query filters, and log levels. It encapsulates the underlying Objective-C OSLogStore functionalities,
+/// facilitating seamless integration within C++ applications.
+class OSLogStoreWrapper
+{
+public:
+    /// @struct LogEntry
+    /// @brief Represents a single log entry with timestamp and message details.
+    ///
+    /// This structure encapsulates the essential components of a log entry, including the timestamp in both
+    /// seconds since the Unix epoch and a formatted date string, as well as the log message itself.
+    struct LogEntry
+    {
+        /// @brief The date and time of the log entry in seconds since the Unix epoch.
+        double dateInSeconds;
+
+        /// @brief The formatted date and time string of the log entry.
+        std::string date;
+
+        /// @brief The log message content.
+        std::string log;
+    };
+
+    /// @enum LogLevel
+    /// @brief Enumerates the possible log levels for filtering log entries.
+    enum LogLevel
+    {
+        Undefined,
+        Debug,
+        Info,
+        Notice,
+        Error,
+        Fault,
+    };
+
+    /// @param startTimeSeconds The start time in seconds since the Unix epoch (e.g., 1672531200.0 for Jan 1, 2023).
+    /// @param query An optional query string using NSPredicate syntax for additional filtering.
+    /// @param logLevel The desired log level to filter entries.
+    ///
+    /// @return A `std::vector` of `LogEntry` structs containing the date and log message for each matching entry.
+    ///
+    /// @throws std::runtime_error If the OSLogStore cannot be accessed or log enumeration fails.
+    std::vector<OSLogStoreWrapper::LogEntry>
+    AllEntries(const double startTimeSeconds, const std::string& query, const LogLevel logLevel = LogLevel::Undefined);
+};
