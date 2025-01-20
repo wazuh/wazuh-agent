019d7bdd78523285a885a1d711281018066542fc
Ariel Martin <ariel.martin@wazuh.com>
2025-01-20 17:11:12 -0300

    feat: adds stderr output to logger
    Removes all sinks and inserts a stderr sink.

--- src/common/logger/src/logger_win.cpp
+++ src/common/logger/src/logger_win.cpp
@@ -10,6 +10,8 @@ Logger::Logger()
     auto sink = std::make_shared<spdlog::sinks::win_eventlog_sink_mt>("Wazuh-Agent");
     auto logger = std::make_shared<spdlog::logger>("wazuh-agent", sink);
 
+    spdlog::register_logger(logger);
+
     spdlog::set_default_logger(logger);
     spdlog::set_level(spdlog::level::info);
     spdlog::cfg::load_env_levels();
