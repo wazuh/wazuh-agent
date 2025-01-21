d6b4f53feb9458fb1932e346418d6fd4048b4110
Ariel Martin <ariel.martin@yahoo.com>
2025-01-21 17:41:08 -0300

    feat: remove unused functions
    Status will be implemente using lock files, without asking SCM for Status

--- src/agent/src/windows/windows_service.cpp
+++ src/agent/src/windows/windows_service.cpp
@@ -79,28 +79,6 @@ namespace
         ReportServiceStatus(g_ServiceStatus.dwCurrentState, NO_ERROR, 0);
     }
 
-    std::string ServiceStatusToString(DWORD currentState)
-    {
-        static const std::unordered_map<DWORD, std::string> statusMap = {
-            {SERVICE_STOPPED, "Service is stopped."},
-            {SERVICE_START_PENDING, "Service is starting..."},
-            {SERVICE_STOP_PENDING, "Service is stopping..."},
-            {SERVICE_RUNNING, "Service is running."},
-            {SERVICE_CONTINUE_PENDING, "Service is resuming..."},
-            {SERVICE_PAUSE_PENDING, "Service is pausing..."},
-            {SERVICE_PAUSED, "Service is paused."}};
-
-        auto it = statusMap.find(currentState);
-        if (it != statusMap.end())
-        {
-            return it->second;
-        }
-        else
-        {
-            return "Unknown service status.";
-        }
-    }
-
     bool GetService(ServiceHandle& hSCManager, ServiceHandle& hService, DWORD desiredAccess)
     {
         hSCManager.reset(OpenSCManager(nullptr, nullptr, SC_MANAGER_ALL_ACCESS));
@@ -301,30 +279,6 @@ namespace WindowsService
         else
         {
             LogInfo("Service {} started successfully.", AGENT_SERVICENAME.c_str());
-        }
-    }
-
-    void ServiceStatus()
-    {
-        ServiceHandle hService;
-        ServiceHandle hSCManager;
-
-        if (!GetService(hSCManager, hService, SERVICE_QUERY_STATUS))
-            return;
-
-        SERVICE_STATUS_PROCESS serviceStatus;
-        DWORD bytesNeeded;
-        if (!QueryServiceStatusEx(hService.get(),
-                                  SC_STATUS_PROCESS_INFO,
-                                  (LPBYTE)&serviceStatus,
-                                  sizeof(SERVICE_STATUS_PROCESS),
-                                  &bytesNeeded))
-        {
-            LogError("Error: Unable to query service status. Error: {}", GetLastError());
-        }
-        else
-        {
-            LogInfo("{}", ServiceStatusToString(serviceStatus.dwCurrentState));
         }
     }
 } // namespace WindowsService
