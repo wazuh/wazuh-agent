d0e398f9af3f94e16fa4dfeea13700d8c5f56f0c
Ariel Martin <ariel.martin@wazuh.com>
2025-02-17 19:22:01 -0300

    feat: implements restart command for macOS

--- src/agent/restart_handler/src/restart_handler_darwin.cpp
+++ src/agent/restart_handler/src/restart_handler_darwin.cpp
@@ -1,14 +1,26 @@
 #include <restart_handler_unix.hpp>
 
+#include <logger.hpp>
+
 namespace restart_handler
 {
     bool RunningAsService()
     {
-        // TODO: Implement this function for macOS
+        return (getppid() == 1);
     }
 
     boost::asio::awaitable<module_command::CommandExecutionResult> RestartService()
     {
-        // TODO: Implement this function for macOS
+        if (std::system("launchctl kickstart -k system/com.wazuh.agent") != 0)
+        {
+            co_return module_command::CommandExecutionResult {module_command::Status::IN_PROGRESS,
+                                                              "launchctl restart execution"};
+        }
+        else
+        {
+            LogError("launchctl failed to restart Agent");
+            co_return module_command::CommandExecutionResult {module_command::Status::FAILURE,
+                                                              "launchctl restart failed"};
+        }
     }
 } // namespace restart_handler
