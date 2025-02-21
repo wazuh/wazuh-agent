668e559b3f57cbcfa3f5ba6d14f4504dfc6c0c47
jr0me <joaquin.romera@wazuh.com>
2025-02-21 11:20:16 -0300

    refactor: enhance documentation for AgentRunner class and its methods

--- src/agent/include/agent_runner.hpp
+++ src/agent/include/agent_runner.hpp
@@ -4,17 +4,30 @@
 
 #include <optional>
 
+/// @brief The AgentRunner class is responsible for running the agent based on the provided command line arguments.
 class AgentRunner
 {
 public:
+    /// @brief Constructor for the AgentRunner class.
+    /// @param argc The number of command line arguments.
+    /// @param argv The command line arguments.
     AgentRunner(int argc, char* argv[]);
 
+    /// @brief Runs the agent based on the provided command line arguments.
+    /// @return The exit code of the agent.
     int Run() const;
 
 private:
+    /// @brief Set the available CLI options and parse the provided CLI arguments.
+    /// @param argc The number of command line arguments.
+    /// @param argv The command line arguments.
     void ParseOptions(int argc, char* argv[]);
+
+    /// @brief Adds platform-specific options to the options description.
     void AddPlatformSpecificOptions();
 
+    /// @brief Handles platform-specific options.
+    /// @return std::nullopt if there were no platform-specific options to handle, their exit code otherwise.
     std::optional<int> HandlePlatformSpecificOptions() const;
 
     /// @brief Registers the agent with the provided command line arguments.
