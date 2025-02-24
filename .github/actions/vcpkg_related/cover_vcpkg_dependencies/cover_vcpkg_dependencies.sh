8de3c02e3be8ffe2a67bdc3920945b8c4bf090fc
Elias Lujan <xkazinx@gmail.com>
2025-02-24 19:47:15 -0300

    fix: Added -y parameter to apt install for auto-confirm

--- .github/actions/vcpkg_related/cover_vcpkg_dependencies/cover_vcpkg_dependencies.sh
+++ .github/actions/vcpkg_related/cover_vcpkg_dependencies/cover_vcpkg_dependencies.sh
@@ -11,14 +11,14 @@ if [[ $(mono --version 2>/dev/null) =~ [0-9] ]]; then
     echo "mono already installed."
 else
     if [[ "$(uname)" == "Linux" ]]; then
-        sudo apt install ca-certificates gnupg
+        sudo apt install -y ca-certificates gnupg
         sudo gpg --homedir /tmp --no-default-keyring \
             --keyring /usr/share/keyrings/mono-official-archive-keyring.gpg \
             --keyserver hkp://keyserver.ubuntu.com:80 \
             --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
         echo "deb [signed-by=/usr/share/keyrings/mono-official-archive-keyring.gpg] https://download.mono-project.com/repo/ubuntu stable-focal main" | sudo tee /etc/apt/sources.list.d/mono-official-stable.list
         sudo apt update
-        sudo apt install mono-devel
+        sudo apt install -y mono-devel
     elif [[ "$(uname)" == "Darwin" ]]; then
         curl -sO https://download.mono-project.com/archive/6.12.0/macos-10-universal/MonoFramework-MDK-6.12.0.206.macos10.xamarin.universal.pkg
         sudo installer -pkg MonoFramework-MDK-6.12.0.206.macos10.xamarin.universal.pkg -target / 
