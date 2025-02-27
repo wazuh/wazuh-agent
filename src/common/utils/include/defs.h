e298926202e696b5373d02c1b24516f8cccff16f
jr0me <joaquin.romera@wazuh.com>
2025-02-27 15:09:05 -0300

    chore: remove unused define

--- src/common/utils/include/defs.h
+++ src/common/utils/include/defs.h
@@ -69,7 +69,6 @@
 
 /* Some global names */
 #define __ossec_name    "Wazuh"
-#define __ossec_version "v5.0.0"
 #define __author        "Wazuh Inc."
 #define __contact       "info@wazuh.com"
 #define __site          "http://www.wazuh.com"
