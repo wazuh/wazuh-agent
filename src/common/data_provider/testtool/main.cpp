8a4eb537bbecd815be12a7154fa62b13bacf3674
Tomas Turina <tomas.turina@wazuh.com>
2025-03-13 19:13:26 +0000

    fix: change format of data_provider testool files

--- src/common/data_provider/testtool/main.cpp
+++ src/common/data_provider/testtool/main.cpp
@@ -1,15 +1,4 @@
-/*
- * Wazuh SysInfo
- * Copyright (C) 2015, Wazuh Inc.
- * February 25, 2021.
- *
- * This program is free software; you can redistribute it
- * and/or modify it under the terms of the GNU General Public
- * License (version 2) as published by the FSF - Free Software
- * Foundation.
- */
-
-#include "cmdLineActions.h"
+#include "cmdLineActions.hpp"
 #include "sysInfo.h"
 #include "sysInfo.hpp"
 #include <iostream>
