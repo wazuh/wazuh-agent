ba54216c397ecb316365a194ce45f0dc9081f995
jr0me <joaquin.romera@wazuh.com>
2025-03-31 11:41:33 -0300

    refactor: comment out DBSync initialization in SCA module for future injection

--- src/modules/sca/include/sca.hpp
+++ src/modules/sca/include/sca.hpp
@@ -30,8 +30,9 @@ public:
         m_dbFilePath = configurationParser->GetConfigOrDefault(config::DEFAULT_DATA_PATH, "agent", "path.data") + "/" +
                        SCA_DB_DISK_NAME;
 
-        m_dBSync = std::make_unique<DBSyncType>(
-            HostType::AGENT, DbEngineType::SQLITE3, m_dbFilePath, "", DbManagement::PERSISTENT);
+        // TODO inject dbsync shared pointer, initialized by module manager
+        // m_dBSync = std::make_unique<DBSyncType>(
+        //     HostType::AGENT, DbEngineType::SQLITE3, m_dbFilePath, "", DbManagement::PERSISTENT);
     }
 
     /// @copydoc ISecurityConfigurationAssessment::~ISecurityConfigurationAssessment
