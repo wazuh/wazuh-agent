9e10d761194c57142dd66537eead90645c3f6722
jr0me <joaquin.romera@wazuh.com>
2025-03-30 22:36:42 -0300

    test: refactor ScaTest to use test fixture for setup

--- src/modules/sca/tests/sca_test.cpp
+++ src/modules/sca/tests/sca_test.cpp
@@ -16,15 +16,28 @@ public:
     }
 };
 
-TEST(ScaTest, Constructor)
+// fixture
+class ScaTest : public ::testing::Test
 {
-    const auto configParser = std::make_shared<const configuration::ConfigurationParser>(std::string(R"(
-        agent:
-          retry_interval: 5
-          verification_mode: none
-        events:
-          batch_size: 1
-    )"));
+protected:
+    void SetUp() override
+    {
+        m_configurationParser = std::make_shared<const configuration::ConfigurationParser>(std::string(R"(
+            agent:
+              retry_interval: 5
+              verification_mode: none
+            events:
+              batch_size: 1
+        )"));
 
-    const SecurityConfigurationAssessment<MockDBSync> sca(configParser);
+        m_sca = std::make_unique<SecurityConfigurationAssessment<MockDBSync>>(m_configurationParser);
+    }
+
+    std::shared_ptr<const configuration::ConfigurationParser> m_configurationParser = nullptr;
+    std::unique_ptr<SecurityConfigurationAssessment<MockDBSync>> m_sca = nullptr;
+};
+
+TEST_F(ScaTest, Constructor)
+{
+    SUCCEED();
 }
