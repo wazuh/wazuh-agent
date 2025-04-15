e966ca297fcaffd7f8632afcf38e4ce9e7c6fd6c
jr0me <joaquin.romera@wazuh.com>
2025-04-15 18:38:48 -0300

    refactor: add CheckConditionEvaluator class to handle conditional rule evaluations

--- src/modules/sca/include/sca_policy_check.hpp
+++ src/modules/sca/include/sca_policy_check.hpp
@@ -25,6 +25,95 @@ enum class RuleResult
     NotFound
 };
 
+enum class ConditionType
+{
+    All,
+    Any,
+    None
+};
+
+class CheckConditionEvaluator
+{
+public:
+    static CheckConditionEvaluator fromString(const std::string& str)
+    {
+        if (str == "all")
+        {
+            return CheckConditionEvaluator {ConditionType::All};
+        }
+        if (str == "any")
+        {
+            return CheckConditionEvaluator {ConditionType::Any};
+        }
+        if (str == "none")
+        {
+            return CheckConditionEvaluator {ConditionType::None};
+        }
+        throw std::invalid_argument("Invalid condition type: " + str);
+    }
+
+    explicit CheckConditionEvaluator(ConditionType type)
+        : m_type {type}
+    {
+    }
+
+    void addResult(bool passed)
+    {
+        if (m_result.has_value())
+        {
+            return;
+        }
+
+        ++m_totalRules;
+        m_passedRules += passed;
+
+        switch (m_type)
+        {
+            case ConditionType::All:
+                if (!passed)
+                {
+                    m_result = false;
+                }
+                break;
+            case ConditionType::Any:
+                if (passed)
+                {
+                    m_result = true;
+                }
+                break;
+            case ConditionType::None:
+                if (passed)
+                {
+                    m_result = false;
+                }
+                break;
+        }
+    }
+
+    bool result() const
+    {
+        if (m_result.has_value())
+        {
+            return *m_result;
+        }
+
+        switch (m_type)
+        {
+            case ConditionType::All: return m_totalRules > 0 && m_passedRules == m_totalRules;
+            case ConditionType::Any: return m_passedRules > 0;
+            case ConditionType::None: return m_passedRules == 0;
+        }
+
+        return false;
+    }
+
+private:
+    ConditionType m_type;
+    int m_totalRules {0};
+    int m_passedRules {0};
+    std::optional<bool> m_result;
+};
+
 class IRuleEvaluator
 {
 public:
