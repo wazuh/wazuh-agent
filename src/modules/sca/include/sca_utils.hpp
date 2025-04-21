7f4296a18e16bc82215452f78b68e2a136f56e94
jr0me <joaquin.romera@wazuh.com>
2025-04-21 10:07:25 -0300

    feat: add function to parse rule type

--- src/modules/sca/include/sca_utils.hpp
+++ src/modules/sca/include/sca_utils.hpp
@@ -10,6 +10,17 @@ namespace sca
 {
     using PolicyVariables = std::unordered_map<std::string, std::vector<std::string>>;
 
+    enum WM_SCA_TYPE
+    {
+        WM_SCA_TYPE_FILE,
+        WM_SCA_TYPE_REGISTRY,
+        WM_SCA_TYPE_PROCESS,
+        WM_SCA_TYPE_DIR,
+        WM_SCA_TYPE_COMMAND
+    };
+
+    std::optional<std::pair<int, std::string>> ParseRuleType(const std::string& input);
+
     std::string RuleWithReplacedVariables(const PolicyVariables& variables, std::string rule);
 
     std::vector<std::filesystem::path> ResolvedPaths(const std::string& ruleWithReplacedVariables);
--- src/modules/sca/src/sca_utils.cpp
+++ src/modules/sca/src/sca_utils.cpp
@@ -2,6 +2,39 @@
 
 namespace sca
 {
+    std::optional<std::pair<int, std::string>> ParseRuleType(const std::string& input)
+    {
+        const auto delimiter_pos = input.find(':');
+
+        if (delimiter_pos == std::string::npos)
+        {
+            return std::nullopt;
+        }
+
+        auto key = input.substr(0, delimiter_pos);
+        const auto value = input.substr(delimiter_pos + 1);
+
+        if (!key.empty() && key.front() == '!')
+        {
+            key.erase(0, 1);
+        }
+
+        static const std::unordered_map<std::string, int> type_map = {{"f", WM_SCA_TYPE_FILE},
+                                                                      {"r", WM_SCA_TYPE_REGISTRY},
+                                                                      {"p", WM_SCA_TYPE_PROCESS},
+                                                                      {"d", WM_SCA_TYPE_DIR},
+                                                                      {"c", WM_SCA_TYPE_COMMAND}};
+
+        const auto it = type_map.find(key);
+
+        if (it == type_map.end())
+        {
+            return std::nullopt;
+        }
+
+        return std::make_pair(it->second, value);
+    }
+
     std::string RuleWithReplacedVariables(const PolicyVariables& variables, std::string rule)
     {
         for (const auto& [var, values] : variables)
