d096a8eebe7194a3dc46ba4247836f3863572541
jr0me <joaquin.romera@wazuh.com>
2025-04-22 12:18:57 -0300

    refactor: simplify policy context struct

--- src/modules/sca/include/sca_policy.hpp
+++ src/modules/sca/include/sca_policy.hpp
@@ -5,14 +5,11 @@
 
 #include <boost/asio/awaitable.hpp>
 
-#include <boost/asio/steady_timer.hpp>
-#include <boost/asio/this_coro.hpp>
-#include <boost/asio/use_awaitable.hpp>
-
 #include <atomic>
 #include <filesystem>
 #include <functional>
 #include <memory>
+#include <string>
 #include <vector>
 
 class SCAPolicy
@@ -27,74 +24,22 @@ public:
     };
 
     /// @brief Constructor
-    SCAPolicy(std::vector<Check> checks)
-        : m_checks(std::move(checks))
-    {
-    }
+    explicit SCAPolicy(std::vector<Check> checks);
 
-    SCAPolicy(SCAPolicy&& other) noexcept
-        : m_checks(std::move(other.m_checks))
-        , m_keepRunning(other.m_keepRunning.load())
-    {
-    }
+    SCAPolicy(SCAPolicy&& other) noexcept;
 
     /// @brief Runs the policy check
     /// @return Awaitable void
-    boost::asio::awaitable<void> Run()
-    {
-        while (m_keepRunning)
-        {
-            auto requirementsOk = false;
+    boost::asio::awaitable<void> Run();
 
-            for (const auto& requirement : m_requirements)
-            {
-                auto resultEvaluator = CheckConditionEvaluator::fromString(requirement.condition);
-
-                for (const auto& rule : requirement.rules)
-                {
-                    resultEvaluator.addResult(rule->Evaluate() == RuleResult::Found);
-                }
-
-                requirementsOk = resultEvaluator.result();
-            }
-
-            if (requirementsOk)
-            {
-                for (const auto& check : m_checks)
-                {
-                    auto resultEvaluator = CheckConditionEvaluator::fromString(check.condition);
-
-                    for (const auto& rule : check.rules)
-                    {
-                        resultEvaluator.addResult(rule->Evaluate() == RuleResult::Found);
-                    }
-
-                    [[maybe_unused]] auto result = resultEvaluator.result();
-                }
-            }
-
-            auto executor = co_await boost::asio::this_coro::executor;
-            boost::asio::steady_timer timer(executor);
-            timer.expires_after(std::chrono::seconds(5));
-            co_await timer.async_wait(boost::asio::use_awaitable);
-        }
-        co_return;
-    }
-
-    void Stop()
-    {
-        m_keepRunning = false;
-    }
+    /// @brief Stops the policy check
+    void Stop();
 
     /// @brief Loads a policy from a SCA Policy yaml file
     /// @param path The path to the SCA Policy yaml file
     /// @returns A SCAPolicy object
     /// @note This function is a placeholder and should be implemented in the actual code
-    static SCAPolicy LoadFromFile([[maybe_unused]] const std::filesystem::path& path)
-    {
-        std::vector<Check> checks;
-        return SCAPolicy(std::move(checks));
-    }
+    static SCAPolicy LoadFromFile([[maybe_unused]] const std::filesystem::path& path);
 
 private:
     std::vector<Check> m_requirements;
---
+++ src/modules/sca/src/sca_policy.cpp
@@ -0,0 +1,68 @@
+#include <sca_policy.hpp>
+
+#include <boost/asio/steady_timer.hpp>
+#include <boost/asio/this_coro.hpp>
+#include <boost/asio/use_awaitable.hpp>
+
+SCAPolicy::SCAPolicy(std::vector<Check> checks)
+    : m_checks(std::move(checks))
+{
+}
+
+SCAPolicy::SCAPolicy(SCAPolicy&& other) noexcept
+    : m_checks(std::move(other.m_checks))
+    , m_keepRunning(other.m_keepRunning.load())
+{
+}
+
+boost::asio::awaitable<void> SCAPolicy::Run()
+{
+    while (m_keepRunning)
+    {
+        auto requirementsOk = false;
+
+        for (const auto& requirement : m_requirements)
+        {
+            auto resultEvaluator = CheckConditionEvaluator::fromString(requirement.condition);
+
+            for (const auto& rule : requirement.rules)
+            {
+                resultEvaluator.addResult(rule->Evaluate() == RuleResult::Found);
+            }
+
+            requirementsOk = resultEvaluator.result();
+        }
+
+        if (requirementsOk)
+        {
+            for (const auto& check : m_checks)
+            {
+                auto resultEvaluator = CheckConditionEvaluator::fromString(check.condition);
+
+                for (const auto& rule : check.rules)
+                {
+                    resultEvaluator.addResult(rule->Evaluate() == RuleResult::Found);
+                }
+
+                [[maybe_unused]] auto result = resultEvaluator.result();
+            }
+        }
+
+        auto executor = co_await boost::asio::this_coro::executor;
+        boost::asio::steady_timer timer(executor);
+        timer.expires_after(std::chrono::seconds(5)); // NOLINT(cppcoreguidelines-avoid-magic-numbers)
+        co_await timer.async_wait(boost::asio::use_awaitable);
+    }
+    co_return;
+}
+
+void SCAPolicy::Stop()
+{
+    m_keepRunning = false;
+}
+
+SCAPolicy SCAPolicy::LoadFromFile([[maybe_unused]] const std::filesystem::path& path)
+{
+    std::vector<Check> checks;
+    return SCAPolicy(std::move(checks));
+}
