3b3f325aeed351333531224d648e9d555f54af2e
jr0me <joaquin.romera@wazuh.com>
2025-04-22 22:56:23 -0300

    feat: get file content and use PatternMatches in FileRuleEvaluator

--- src/modules/sca/include/sca_policy_check.hpp
+++ src/modules/sca/include/sca_policy_check.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <ifile_io_utils.hpp>
 #include <ifilesystem_wrapper.hpp>
 #include <sca_utils.hpp>
 
@@ -49,7 +50,9 @@ protected:
 
 class FileRuleEvaluator : public RuleEvaluator
 {
-    FileRuleEvaluator(PolicyEvaluationContext ctx, std::unique_ptr<IFileSystemWrapper> fileSystemWrapper);
+    FileRuleEvaluator(PolicyEvaluationContext ctx,
+                      std::unique_ptr<IFileSystemWrapper> fileSystemWrapper,
+                      std::unique_ptr<IFileIOUtils> fileUtils);
 
     RuleResult Evaluate() override;
 
@@ -57,6 +60,8 @@ private:
     RuleResult CheckFileForContents();
 
     RuleResult CheckFileExistence();
+
+    std::unique_ptr<IFileIOUtils> m_fileUtils = nullptr;
 };
 
 class CommandRuleEvaluator : public RuleEvaluator
