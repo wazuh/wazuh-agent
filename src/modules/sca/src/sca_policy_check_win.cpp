da7a4ecbdd7b731bce024603f0e62acb0541f05a
jr0me <joaquin.romera@wazuh.com>
2025-04-24 11:47:11 -0300

    feat: implement registry evaluation logic in RegistryRuleEvaluator

--- src/modules/sca/src/sca_policy_check_win.cpp
+++ src/modules/sca/src/sca_policy_check_win.cpp
@@ -1,5 +1,10 @@
 #include <sca_policy_check.hpp>
 
+#include <registryHelper.hpp>
+#include <sca_utils.hpp>
+
+#include <logger.hpp>
+
 RegistryRuleEvaluator::RegistryRuleEvaluator(PolicyEvaluationContext ctx,
                                              std::unique_ptr<IFileSystemWrapper> fileSystemWrapper)
     : RuleEvaluator(std::move(ctx), std::move(fileSystemWrapper))
@@ -8,5 +13,47 @@ RegistryRuleEvaluator::RegistryRuleEvaluator(PolicyEvaluationContext ctx,
 
 RuleResult RegistryRuleEvaluator::Evaluate()
 {
-    return RuleResult::Invalid;
+    try
+    {
+        if (m_ctx.pattern)
+        {
+            if (const auto content = sca::GetPattern(*m_ctx.pattern))
+            {
+                const auto registryKey = m_ctx.pattern->substr(0, m_ctx.pattern->find(" -> "));
+
+                Utils::Registry registry(m_ctx.rule, registryKey);
+                for (const auto& value : registry.enumerateValueKey())
+                {
+                    if (value == *content)
+                    {
+                        return RuleResult::Found;
+                    }
+                }
+                return RuleResult::NotFound;
+            }
+            else
+            {
+                Utils::Registry registry(m_ctx.rule, *m_ctx.pattern);
+                for (const auto& key : registry.enumerate())
+                {
+                    if (key == *m_ctx.pattern)
+                    {
+                        return RuleResult::Found;
+                    }
+                }
+
+                return RuleResult::NotFound;
+            }
+        }
+
+        // If there's no pattern we are just checking that the registry exists
+        // Since Utils::Registry will throw if it doesn't exist, we can just return Found
+        [[maybe_unused]] Utils::Registry registry(m_ctx.rule);
+        return RuleResult::Found;
+    }
+    catch (const std::exception& e)
+    {
+        LogDebug("RegistryRuleEvaluator::Evaluate: Exception: {}", e.what());
+    }
+    return RuleResult::NotFound;
 }
