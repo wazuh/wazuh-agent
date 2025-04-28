3163c15e95a96e0fb3ce884c53bb3cbee6d29749
jr0me <joaquin.romera@wazuh.com>
2025-04-28 20:48:43 -0300

    fix: enhance file existence checks to include regular file validation in FileRuleEvaluator

--- src/modules/sca/tests/file_rule_evaluator_test.cpp
+++ src/modules/sca/tests/file_rule_evaluator_test.cpp
@@ -49,6 +49,7 @@ TEST_F(FileRuleEvaluatorTest, FileExistsReturnsFound)
     m_ctx.rule = "some/file";
 
     EXPECT_CALL(*m_rawFsMock, exists(std::filesystem::path("some/file"))).WillOnce(::testing::Return(true));
+    EXPECT_CALL(*m_rawFsMock, is_regular_file(std::filesystem::path("some/file"))).WillOnce(::testing::Return(true));
 
     auto evaluator = CreateEvaluator();
     EXPECT_EQ(evaluator.Evaluate(), RuleResult::Found);
@@ -60,6 +61,7 @@ TEST_F(FileRuleEvaluatorTest, PatternRegexMatchesContentReturnsFound)
     m_ctx.rule = "some/file";
 
     EXPECT_CALL(*m_rawFsMock, exists(std::filesystem::path("some/file"))).WillOnce(::testing::Return(true));
+    EXPECT_CALL(*m_rawFsMock, is_regular_file(std::filesystem::path("some/file"))).WillOnce(::testing::Return(true));
     EXPECT_CALL(*m_rawIoMock, getFileContent("some/file")).WillOnce(::testing::Return("foo"));
 
     auto evaluator = CreateEvaluator();
@@ -72,6 +74,7 @@ TEST_F(FileRuleEvaluatorTest, PatternRegexDoesNotMatchContentReturnsNotFound)
     m_ctx.rule = "some/file";
 
     EXPECT_CALL(*m_rawFsMock, exists(std::filesystem::path("some/file"))).WillOnce(::testing::Return(true));
+    EXPECT_CALL(*m_rawFsMock, is_regular_file(std::filesystem::path("some/file"))).WillOnce(::testing::Return(true));
     EXPECT_CALL(*m_rawIoMock, getFileContent("some/file")).WillOnce(::testing::Return("bar"));
 
     auto evaluator = CreateEvaluator();
@@ -84,6 +87,7 @@ TEST_F(FileRuleEvaluatorTest, PatternExactLineMatchesReturnsFound)
     m_ctx.rule = "some/file";
 
     EXPECT_CALL(*m_rawFsMock, exists(std::filesystem::path("some/file"))).WillOnce(::testing::Return(true));
+    EXPECT_CALL(*m_rawFsMock, is_regular_file(std::filesystem::path("some/file"))).WillOnce(::testing::Return(true));
     EXPECT_CALL(*m_rawIoMock, readLineByLine(std::filesystem::path("some/file"), ::testing::_))
         .WillOnce(::testing::Invoke(
             [](const std::filesystem::path&, const std::function<bool(const std::string&)>& callback)
@@ -102,6 +106,7 @@ TEST_F(FileRuleEvaluatorTest, PatternExactLineNoMatchReturnsNotFound)
     m_ctx.rule = "some/file";
 
     EXPECT_CALL(*m_rawFsMock, exists(std::filesystem::path("some/file"))).WillOnce(::testing::Return(true));
+    EXPECT_CALL(*m_rawFsMock, is_regular_file(std::filesystem::path("some/file"))).WillOnce(::testing::Return(true));
     EXPECT_CALL(*m_rawIoMock, readLineByLine(std::filesystem::path("some/file"), ::testing::_))
         .WillOnce(::testing::Invoke(
             [](const std::filesystem::path&, const std::function<bool(const std::string&)>& callback)
@@ -114,7 +119,7 @@ TEST_F(FileRuleEvaluatorTest, PatternExactLineNoMatchReturnsNotFound)
     EXPECT_EQ(evaluator.Evaluate(), RuleResult::NotFound);
 }
 
-TEST_F(FileRuleEvaluatorTest, PatternGivenButFileDoesNotExistReturnsNotFound)
+TEST_F(FileRuleEvaluatorTest, PatternGivenButFileDoesNotExistReturnsInvalid)
 {
     m_ctx.pattern = std::string("r:foo");
     m_ctx.rule = "some/file";
@@ -122,5 +127,17 @@ TEST_F(FileRuleEvaluatorTest, PatternGivenButFileDoesNotExistReturnsNotFound)
     EXPECT_CALL(*m_rawFsMock, exists(std::filesystem::path("some/file"))).WillOnce(::testing::Return(false));
 
     auto evaluator = CreateEvaluator();
-    EXPECT_EQ(evaluator.Evaluate(), RuleResult::NotFound);
+    EXPECT_EQ(evaluator.Evaluate(), RuleResult::Invalid);
+}
+
+TEST_F(FileRuleEvaluatorTest, PatternGivenButPathIsNotRegularFileReturnsInvalid)
+{
+    m_ctx.pattern = std::string("r:foo");
+    m_ctx.rule = "some/file";
+
+    EXPECT_CALL(*m_rawFsMock, exists(std::filesystem::path("some/file"))).WillOnce(::testing::Return(true));
+    EXPECT_CALL(*m_rawFsMock, is_regular_file(std::filesystem::path("some/file"))).WillOnce(::testing::Return(false));
+
+    auto evaluator = CreateEvaluator();
+    EXPECT_EQ(evaluator.Evaluate(), RuleResult::Invalid);
 }
