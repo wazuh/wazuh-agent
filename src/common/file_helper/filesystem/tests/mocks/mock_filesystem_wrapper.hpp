fb6f229107c3d847907ef69eb369380c4fe11c13
jr0me <joaquin.romera@wazuh.com>
2025-05-08 16:57:39 -0300

    feat: add is_symlink and canonical methods to IFileSystemWrapper and FileSystemWrapper

--- src/common/file_helper/filesystem/tests/mocks/mock_filesystem_wrapper.hpp
+++ src/common/file_helper/filesystem/tests/mocks/mock_filesystem_wrapper.hpp
@@ -9,6 +9,8 @@ public:
     MOCK_METHOD(bool, is_directory, (const std::filesystem::path& path), (const, override));
     MOCK_METHOD(bool, is_regular_file, (const std::filesystem::path& path), (const, override));
     MOCK_METHOD(bool, is_socket, (const std::filesystem::path& path), (const, override));
+    MOCK_METHOD(bool, is_symlink, (const std::filesystem::path& path), (const, override));
+    MOCK_METHOD(std::filesystem::path, canonical, (const std::filesystem::path& path), (const, override));
     MOCK_METHOD(std::uintmax_t, remove_all, (const std::filesystem::path& path), (const, override));
     MOCK_METHOD(std::filesystem::path, temp_directory_path, (), (const, override));
     MOCK_METHOD(bool, create_directories, (const std::filesystem::path& path), (const, override));
